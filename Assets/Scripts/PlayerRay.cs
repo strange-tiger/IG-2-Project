45b99a8b7dec3a8e6edcceeaaba97a0efc237036
qwas4689 <qwas4689@gmail.com>
2022-10-07 17:44:13 +0900

    [Fix] PlayerRay 코드 수정
    LineRenderer Material 추가해주는 코드 작성

--- Assets/Scripts/PlayerRay.cs
+++ Assets/Scripts/PlayerRay.cs
@@ -9,11 +9,17 @@ public class PlayerRay : MonoBehaviour
 
     private LineRenderer _lineRenderer;
 
+    private Vector3[] _rayPositions = new Vector3[2];
+    private Color _startRayColor = new Vector4(42f, 244f, 37f);
+    private Color _endRayColor = new Vector4(42f, 244f, 37f);
+
+    private float _rayLength = 5.0f;
+    private float alpha = 1.0f;
+
     void Awake()
     {
         _lineRenderer = GetComponentInChildren<LineRenderer>();
-
-        _lineRenderer.positionCount = 2;
+        SetRayColor();
         _lineRenderer.enabled = false;
     }
 
@@ -26,9 +32,6 @@ public class PlayerRay : MonoBehaviour
     {
         if (_playerInput.isRay)
         {
-            _lineRenderer.SetPosition(0, Vector3.zero);
-            _lineRenderer.SetPosition(1, new Vector3(0f, 0f, 5f));
-
             _lineRenderer.enabled = true;
 
             Ray ray;
@@ -42,5 +45,24 @@ public class PlayerRay : MonoBehaviour
         {
             _lineRenderer.enabled = false;
         }
+    }
+
+    private void SetRayColor()
+    {
+        _lineRenderer.material = new Material(Shader.Find("RayColor"));
+
+        _rayPositions[0] = transform.position;
+        _rayPositions[1] = transform.position + transform.forward * _rayLength;
+
+        _lineRenderer.positionCount = _rayPositions.Length;
+        _lineRenderer.SetPositions(_rayPositions);
+
+        Gradient RayMaterialGradient = new Gradient();
+
+        RayMaterialGradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(_startRayColor, 0.0f), new GradientColorKey(_endRayColor, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(0.0f, 0.0f) }
+            );
+        _lineRenderer.colorGradient = RayMaterialGradient;
     }
 }
