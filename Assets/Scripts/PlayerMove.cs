464aa03a440fa57ea77050a15bc51c1510147d68
qwas4689 <qwas4689@gmail.com>
2022-10-08 10:44:37 +0900

    [Fix] PlayerMove 리펙토링 및 코드컨벤션 수정
    회전을
    transform.Rotate(thumbstick * _rotateSpeed); 으로 수정
    코드컨벤션 수정

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,8 +6,8 @@ public class PlayerMove : MonoBehaviour
 {
     private CharacterController _controller;
 
-    [SerializeField]
-    private float speed = 3.0f;
+    private float _speed = 3.0f;
+    private float _rotateSpeed = 2.0f;
 
     private void Start()
     {
@@ -25,8 +25,8 @@ public class PlayerMove : MonoBehaviour
         {
             Vector2 thumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 
-            float inputX = thumbstick.x * speed * Time.deltaTime;
-            float inputZ = thumbstick.y * speed * Time.deltaTime;
+            float inputX = thumbstick.x * _speed * Time.deltaTime;
+            float inputZ = thumbstick.y * _speed * Time.deltaTime;
 
             _controller.Move(new Vector3(inputX, 0f, inputZ));
         }
@@ -38,23 +38,25 @@ public class PlayerMove : MonoBehaviour
         {
             Vector2 thumbstick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
 
-            if (thumbstick.x < 0)
-            {
-                gameObject.transform.Rotate(0, thumbstick.x * 2, 0);
-            }
-            else if (thumbstick.x > 0)
-            {
-                gameObject.transform.Rotate(0, thumbstick.x * 2, 0);
-            }
+            transform.Rotate(thumbstick * _rotateSpeed);
 
-            if (thumbstick.y < 0)
-            {
-                gameObject.transform.Rotate(0, thumbstick.y, 0);
-            }
-            else if (thumbstick.y > 0)
-            {
-                gameObject.transform.Rotate(0, thumbstick.y, 0);
-            }
+            //if (thumbstick.x < 0)
+            //{
+            //    gameObject.transform.Rotate(0, thumbstick.x * 2, 0);
+            //}
+            //else if (thumbstick.x > 0)
+            //{
+            //    gameObject.transform.Rotate(0, thumbstick.x * 2, 0);
+            //}
+
+            //if (thumbstick.y < 0)
+            //{
+            //    gameObject.transform.Rotate(0, thumbstick.y, 0);
+            //}
+            //else if (thumbstick.y > 0)
+            //{
+            //    gameObject.transform.Rotate(0, thumbstick.y, 0);
+            //}
         }
     }
 }
