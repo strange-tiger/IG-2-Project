130b6880a18e4bab9753e9d66f505dd4c74a63fc
qwas4689 <qwas4689@gmail.com>
2022-10-11 14:30:14 +0900

    [Feat] 플레이어가 바라보는 방향으로 몸 회전 기능 구현
    테스트를 거쳐야 함

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
     private PlayerInput _playerInput;
     private CharacterController _controller;
+    private Transform _cam;
 
     private float _speed = 3.0f;
-    private float _rotateSpeed = 2.0f;
 
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
         _playerInput = GetComponent<PlayerInput>();
+        _cam = Camera.main.GetComponent<Transform>();
     }
 
     private void Update()
     {
         MovePlayer();
-        RotatePlayer();
     }
     private void MovePlayer()
     {
@@ -30,35 +30,9 @@ public class PlayerMovement : MonoBehaviour
             float inputX = thumbstick.x * _speed * Time.deltaTime;
             float inputZ = thumbstick.y * _speed * Time.deltaTime;
 
-            _controller.Move(new Vector3(inputX, 0f, inputZ));
-        }
-    }
+            Vector3 playerLook = _cam.TransformDirection(Vector3.forward);
 
-    private void RotatePlayer()
-    {
-        if (_playerInput.IsRotate)
-        {
-            Vector2 thumbstick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
-
-            transform.Rotate(thumbstick * _rotateSpeed);
-
-            //if (thumbstick.x < 0)
-            //{
-            //    gameObject.transform.Rotate(0, thumbstick.x * 2, 0);
-            //}
-            //else if (thumbstick.x > 0)
-            //{
-            //    gameObject.transform.Rotate(0, thumbstick.x * 2, 0);
-            //}
-
-            //if (thumbstick.y < 0)
-            //{
-            //    gameObject.transform.Rotate(0, thumbstick.y, 0);
-            //}
-            //else if (thumbstick.y > 0)
-            //{
-            //    gameObject.transform.Rotate(0, thumbstick.y, 0);
-            //}
+            _controller.Move(new Vector3(inputX, 0f, inputZ) + playerLook);
         }
     }
 }
