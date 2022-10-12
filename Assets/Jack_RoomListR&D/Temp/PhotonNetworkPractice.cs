d8ce3796360a4cf1e3f9d87a2c00c69c35ed4b86
csy-59 <csy010509@naver.com>
2022-10-12 10:55:50 +0900

    [Refac] 스크립트 정리
    - 스크립트 정리
    - 중요 함수에 주석 작성

--- Assets/Jack_RoomListR&D/Temp/PhotonNetworkPractice.cs
+++ Assets/Jack_RoomListR&D/Temp/PhotonNetworkPractice.cs
@@ -20,59 +20,64 @@ public class PhotonNetworkPractice : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject _roomPanel;
     [SerializeField] private GameObject _scrollViewContent;
 
+    private RoomOptions _roomOption = new RoomOptions()
+    {
+        MaxPlayers = 5
+    };
+
+    // 여기에 RoomList가 저장됨.
     private Dictionary<string, RoomInfo> cachedRoomList = new Dictionary<string, RoomInfo>();
     private int _roomCount = 0;
 
+    /* 서버/로비 접속 관련 함수 */
     private void Awake()
     {
         ResetButton();
+
         _infoText.text = "Connecting...";
+        
         PhotonNetwork.ConnectUsingSettings();
     }
-
-    private void ResetButton()
-    {
-        _createRoomButton.onClick.AddListener(() => { CreateRoom(); });
-        _resetRoomListButton.onClick.AddListener(() => { ResetRoomList(); });
-        _exitRoomButton.onClick.AddListener(() => { ExitRoom(); });
-
-        SetInteractableOfAllButtons(false);
-
-        _exitRoomButton.gameObject.SetActive(false);
-    }
-
-    private void SetInteractableOfAllButtons(bool value)
-    {
-        _createRoomButton.interactable = value;
-        _resetRoomListButton.interactable = value;
-        _exitRoomButton.interactable = value;
-    }
-
     public override void OnConnectedToMaster()
     {
         _infoText.text = "Connected! Joinning Lobby...";
         PhotonNetwork.JoinLobby();
     }
-
     public override void OnJoinedLobby()
     {
         _infoText.text = "Lobby Joined!";
         SetInteractableOfAllButtons(true);
     }
+    public override void OnLeftLobby()
+    {
+        _infoText.text = "Reconnecting Lobby...";
+        SetInteractableOfAllButtons(false);
+        PhotonNetwork.JoinLobby();
+    }
 
+
+    /// <summary>
+    /// 방 리스트가 업데이트 될 때마다 호출되는 함수.
+    /// 로비에 있을 때만 실행된다.
+    /// </summary>
+    /// <param name="roomList"></param>
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
         _infoText.text = "Room List Updated";
-        Invoke("setInfoText", 0.5f);
         RoomListUpdate(roomList);
+        Invoke("setInfoText", 0.5f);
     }
-
+    /// <summary>
+    /// cachedRoomList를 초기화 함
+    /// </summary>
+    /// <param name="roomList">OnRoomListUpdate에서 받아온 roomList</param>
     private void RoomListUpdate(List<RoomInfo> roomList)
     {
         Debug.Log(roomList.Count);
 
         foreach (RoomInfo room in roomList)
         {
+            // 방 리스트에 있는 방들 중 이미 삭제 된 방들은 cachedRoomList에서 제외된다.
             if(room.RemovedFromList)
             {
                 cachedRoomList.Remove(room.Name);
@@ -85,8 +90,13 @@ public class PhotonNetworkPractice : MonoBehaviourPunCallbacks
 
         SetRoomList();
     }
+    /// <summary>
+    /// cachedRoomList를 기반으로 UI를 보여줌.
+    /// 효율적이지 못한 스크립트... 발전 필요
+    /// </summary>
     private void SetRoomList()
     {
+        // Room List Panel 초기화
         foreach(Transform roomPanel in _scrollViewContent.GetComponentsInChildren<Transform>())
         {
             if(roomPanel.gameObject == _scrollViewContent.gameObject)
@@ -97,8 +107,10 @@ public class PhotonNetworkPractice : MonoBehaviourPunCallbacks
             Destroy(roomPanel.gameObject);
         }
 
+        // cachedRoomList에 맞춰 Panel 설정
         foreach(KeyValuePair<string, RoomInfo> room in cachedRoomList)
         {
+            // 해당 방이 이미 삭제되었다면 ui에서 제외함
             if(room.Value.RemovedFromList)
             {
                 continue;
@@ -110,19 +122,9 @@ public class PhotonNetworkPractice : MonoBehaviourPunCallbacks
 
         _roomCount = cachedRoomList.Count;
     }
-
-    public override void OnLeftLobby()
-    {
-        _infoText.text = "Reconnecting Lobby...";
-        SetInteractableOfAllButtons(false);
-        PhotonNetwork.JoinLobby();
-    }
-
-    private void setInfoText()
-    {
-        _infoText.text = "...";
-    }
-
+    /// <summary>
+    /// 방 새로 고침
+    /// </summary>
     private void ResetRoomList()
     {
         SetInteractableOfAllButtons(false);
@@ -135,18 +137,9 @@ public class PhotonNetworkPractice : MonoBehaviourPunCallbacks
         Invoke("setInfoText", 0.5f);
     }
 
-    private void ExitRoom()
-    {
-        PhotonNetwork.LeaveRoom();
-        _createRoomButton.gameObject.SetActive(true);
-        _resetRoomListButton.gameObject.SetActive(true);
-        _exitRoomButton.gameObject.SetActive(false);
-    }
 
-    private RoomOptions _roomOption = new RoomOptions()
-    {
-        MaxPlayers = 5
-    };
+
+    /* 방만들기 관련 함수 */
     private void CreateRoom()
     {
         ++_roomCount;
@@ -154,17 +147,45 @@ public class PhotonNetworkPractice : MonoBehaviourPunCallbacks
         _infoText.text = "Creating Room...";
         SetRoomList();
     }
-
     public override void OnCreatedRoom()
     {
         _infoText.text = $"Created Room {PhotonNetwork.CurrentRoom.Name}";
+        
         _createRoomButton.gameObject.SetActive(false);
         _resetRoomListButton.gameObject.SetActive(false);
         _exitRoomButton.gameObject.SetActive(true);
     }
-
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
-        Debug.Log($"Fail to Created Room {_roomCount}");
+        _infoText.text = $"Fail to Created Room {_roomCount}";
+    }
+
+    /* 기타 함수, 중요하지 않음 */
+    private void ExitRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        _createRoomButton.gameObject.SetActive(true);
+        _resetRoomListButton.gameObject.SetActive(true);
+        _exitRoomButton.gameObject.SetActive(false);
+    }
+    private void ResetButton()
+    {
+        _createRoomButton.onClick.AddListener(() => { CreateRoom(); });
+        _resetRoomListButton.onClick.AddListener(() => { ResetRoomList(); });
+        _exitRoomButton.onClick.AddListener(() => { ExitRoom(); });
+
+        SetInteractableOfAllButtons(false);
+
+        _exitRoomButton.gameObject.SetActive(false);
+    }
+    private void SetInteractableOfAllButtons(bool value)
+    {
+        _createRoomButton.interactable = value;
+        _resetRoomListButton.interactable = value;
+        _exitRoomButton.interactable = value;
+    }
+    private void ResetInfoText()
+    {
+        _infoText.text = "Waiting...";
     }
 }
