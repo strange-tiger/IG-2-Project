41a5a3e470cc7818a8ce96f58e424b0482ccf3db
qwas4689 <qwas4689@gmail.com>
2022-10-12 11:48:18 +0900

    [Feat] 메뉴 UI 틀 제작 완료 및 UTF-8 추가

--- Assets/Knight/Scripts_Knight/Buttons.cs
+++ Assets/Knight/Scripts_Knight/Buttons.cs
@@ -1,11 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class Buttons : MonoBehaviour
 {
-
+    // Test
+    // 주석 테스트
     private void Start()
     {
         
