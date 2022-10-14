54a2c88d1fce6a845f482134b8d08b90981caad3
yejin9337 <yejin9337@naver.com>
2022-10-14 21:24:35 +0900

    [Feat] SoundManager 구현 중
    1. SoundManager 구현하는 중
    bgmVolume, sfxVolume 추가
    2. VolumeController 의 BackGroundValueChanged와 EffectValueChanged에서 bgmVolume, sfxVolume 값 변경 코드 추가

--- Assets/Ace/Script/SoundPanel/SoundManager.cs
+++ Assets/Ace/Script/SoundPanel/SoundManager.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // private AudioSource[] _audioSources = new AudioSource[(int)Defines.ESoundType.MaxCount];
-    // private Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
-
     private static SoundManager _instance;
     public static SoundManager Instance
     {
@@ -14,9 +11,28 @@ public class SoundManager : MonoBehaviour
         {
             if (_instance == null)
             {
-                _instance = new SoundManager();
+                _instance = FindObjectOfType<SoundManager>();
             }
             return _instance;
         }
-    }  
+    }
+
+    [SerializeField]
+    private AudioSource _sfxPlayer;
+    [SerializeField]
+    private AudioSource _bgmPlayer;
+
+    [SerializeField]
+    private AudioClip _bgmClip;
+    [SerializeField]
+    private AudioClip[] _sfxClip;
+
+    private float _sfxVolume = 1f;
+    public float SFXVolume { get { return _sfxVolume; } set { _sfxVolume = value; } }
+    private float _bgmVolume = 1f;
+    public float BGMVolume { get { return _bgmVolume; } set { _bgmVolume = value; } }
+
+    // private AudioSource[] _audioSources = new AudioSource[(int)Defines.ESoundType.MaxCount];
+    private Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
+
 }
