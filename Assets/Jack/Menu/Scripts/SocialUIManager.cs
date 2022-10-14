00bf1fdd92b9678d3cbeaadf791d4eb1656aadd8
csy-59 <csy010509@naver.com>
2022-10-14 21:20:43 +0900

    [Feat] Button addListener 함수 추가
    - 버튼에 리스너를 추가해주는 함수 제작

--- Assets/Jack/Menu/Scripts/SocialUIManager.cs
+++ Assets/Jack/Menu/Scripts/SocialUIManager.cs
@@ -12,16 +12,25 @@ public class SocialUIManager : MonoBehaviour
 
     [Header("Add Friend")]
     [SerializeField] private Button _addFriendButton;
+    [SerializeField] private string _requestFriendConfirmMessage;
+    [SerializeField] private string _cancelRequestConfirmMessage;
+    [SerializeField] private string _cancelFriendConfirmMessage;
+
+    [Header("Check Request")]
+    [SerializeField] private string _checkRequestMessage;
+    [SerializeField] private string _acceptRequestMessage;
+    [SerializeField] private string _denyRequestMessage;
 
     [Header("Block User")]
     [SerializeField] private Button _blockFriendButton;
-    [SerializeField] private string _blockInfoString;
-    [SerializeField] private string _blockDissableInfoString;
+    [SerializeField] private string _blockConfirmMessage;
+    [SerializeField] private string _unblockConfirmMessage;
     private TextMeshProUGUI _blockFriendText;
     [SerializeField] private GameObject _blockedConfirmedPanel;
 
     [Header("Extra")]
     [SerializeField] private ConfirmPanelManager _confirmPanelManager;
+    [SerializeField] private CheckPanelManager _checkPanelManager;
 
     private string _myNickname;
     private string _targetUserNickname;
@@ -42,28 +51,28 @@ public class SocialUIManager : MonoBehaviour
         // 1. 둘 사이의 관계를 확인함
 
         // 1-1. 관계가 없는 경우(반환값이 -1)
-        RequestFriendButton();
-        BlockUserButton();
+        OnClickRequestFriendButton();
+        OnClickBlockUserButton();
 
         // 1-2. 친구인 경우(반환값이 0)
-        CancelFriendButton();
-        BlockUserButton();
+        OnClickCancelFriendButton();
+        OnClickBlockUserButton();
 
         // ....그외
         // 2. 둘 사이에서 나의 위치(A인지 B인지 알아냄)를 확인하여
         // 위치에 따라 확인할 비트 자리수를 기준을 설정, 해당 결과로 다음을 판별
-        
+
         // 2-1. 내가 블록한 상황
         ;
-        UnblockUserButton();
+        OnClickUnblockUserButton();
 
         // 2-2. 내가 친구 요청을 한 상황
-        CancelRequestButton();
-        BlockUserButton();
+        OnClickCancelRequestButton();
+        OnClickBlockUserButton();
 
         // 2-3. 상태가 나에게 친구 요청을 한 상황
-        CheckFriendRequestButton();
-        BlockUserButton();
+        OnClickCheckFriendRequestButton();
+        OnClickBlockUserButton();
     }
 
     /// <summary>
@@ -77,32 +86,61 @@ public class SocialUIManager : MonoBehaviour
         _socialUI.SetActive(true);
     }
 
-    private void RequestFriendButton()
+    private void AddListenerToButton(Button button, UnityEngine.Events.UnityAction newListener, bool isNeedButtonInitialize)
     {
-
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(newListener);
+        button.onClick.AddListener(() => { InitializeButtons(); });
     }
 
-    private void CancelRequestButton()
+    // 친구 추가 요청
+    private void OnClickRequestFriendButton()
     {
-
+        // 친구 추가 요청을 DB에 올림
+        _confirmPanelManager.ShowConfirmPanel(_requestFriendConfirmMessage);
     }
 
-    private void CancelFriendButton()
+    // 친구 추가 요청 취소
+    private void OnClickCancelRequestButton()
     {
-
+        // 친구 취소 요청을 DB에 올림
+        _confirmPanelManager.ShowConfirmPanel(_cancelRequestConfirmMessage);
     }
 
-    private void CheckFriendRequestButton()
+    // 친구 삭제
+    private void OnClickCancelFriendButton()
     {
+        // 친구 삭제 요청을 DB에 올림
+        _confirmPanelManager.ShowConfirmPanel(_cancelFriendConfirmMessage);
+    }
 
+    // 상대의 친구 요청 판단
+    private void OnClickCheckFriendRequestButton()
+    {
+        _checkPanelManager.ShowCheckPanel(_targetUserNickname + _checkRequestMessage,
+            () =>
+            {
+                // 친구 추가 요청을 DB에 올림
+                _confirmPanelManager.ShowConfirmPanel(_acceptRequestMessage);
+                InitializeButtons();
+            },
+            () =>
+            {
+                // 친구 삭제 요청을 DB에 올림
+                _confirmPanelManager.ShowConfirmPanel(_denyRequestMessage);
+                InitializeButtons();
+            }
+            );
     }
     
-    private void BlockUserButton()
+    // 상대 차단
+    private void OnClickBlockUserButton()
     {
 
     }
 
-    private void UnblockUserButton()
+    // 상대 차단 해제
+    private void OnClickUnblockUserButton()
     {
 
     }
