5a0401375b7cf3aa4498f103d697b63270efad40
strange-tiger <jinolee9525@gmail.com>
2022-10-15 16:29:30 +0900

    [Fix] 함수 static 한정자 추가
    - 메소드를 모두 정적 메소드로 설정하기 위해 static 한정자 추가

--- Assets/Scripts/Util/DBUtil.cs
+++ Assets/Scripts/Util/DBUtil.cs
@@ -331,7 +331,7 @@ namespace Asset.MySql
         /// <param name="userA"> UserA Column에 들어가는 유저 닉네임. </param>
         /// <param name="userB"> UserB Column에 들어가는 유저 닉네임. </param>
         /// <returns> 존재하면 true, 존재하지 않는다면 false 반환. </returns>
-        private bool IsThereRelationship(string userA, string userB)
+        private static bool IsThereRelationship(string userA, string userB)
         {
             using (MySqlConnection _mysqlConnection = new MySqlConnection(_connectionString))
             {
@@ -364,7 +364,7 @@ namespace Asset.MySql
         /// <param name="targetNickname"> 대상의 닉네임 </param>
         /// <param name="isLeft">내가 UserA라면 True, UserB라면 false</param>
         /// <returns>Row가 존재하면 True, 존재하지 않으면 false </returns>
-        public bool CheckMyPositionInRelationShip(string myNickname, string targetNickname, out bool isLeft)
+        public static bool CheckMyPositionInRelationShip(string myNickname, string targetNickname, out bool isLeft)
         {
 
             using (MySqlConnection _mysqlConnection = new MySqlConnection(_connectionString))
@@ -409,7 +409,7 @@ namespace Asset.MySql
         /// <param name="targetNickname"> 대상 유저의 닉네임 </param>
         /// <param name="isLeft"> 내가 UserA Column이라면 true, UserB Column이라면 false. </param>
         /// <returns> 나와 대상 유저간의 State를 int로 반환함. 관계가 존재하지 않는다면 -1 반환. </returns>
-        public int CheckRelationship(string myNickname, string targetNickname, out bool isLeft)
+        public static int CheckRelationship(string myNickname, string targetNickname, out bool isLeft)
         {
             if (CheckMyPositionInRelationShip(myNickname, targetNickname, out isLeft) == false)
             {
@@ -450,7 +450,7 @@ namespace Asset.MySql
         /// <param name="userB"> UserB column에 들어가는 닉네임. </param>
         /// <param name="state">업데이트할 State </param>
         /// <returns>성공하면 true, 실패하면 false를 반환함. </returns>
-        public bool UpdateRelationship(string userA, string userB, int state)
+        public static bool UpdateRelationship(string userA, string userB, int state)
         {
             if (IsThereRelationship(userA, userB) == false)
             {
@@ -502,7 +502,7 @@ namespace Asset.MySql
         /// <param name="myNickname"> 나의 닉네임 </param>
         /// <param name="targetNickname"> 대상 유저의 닉네임 </param>
         /// <returns>성공하면 true, 실패하면 false를 반환함. </returns>
-        public bool UpdateRelationshipToBlock(string myNickname, string targetNickname)
+        public static bool UpdateRelationshipToBlock(string myNickname, string targetNickname)
         {
             try
             {
@@ -526,7 +526,7 @@ namespace Asset.MySql
         /// <param name="myNickname"> 나의 닉네임 </param>
         /// <param name="targetNickname"> 대상 유저의 닉네임 </param>
         /// <returns>성공하면 true, 실패하면 false를 반환함. </returns>
-        public bool UpdateRalationshipToUnblock(string myNickname, string targetNickname)
+        public static bool UpdateRalationshipToUnblock(string myNickname, string targetNickname)
         {
             try
             {
@@ -551,7 +551,7 @@ namespace Asset.MySql
         /// <param name="myNickname"></param>
         /// <param name="targetNickname"></param>
         /// <returns></returns>
-        public bool UpdateRelationshipToRequest(string myNickname, string targetNickname)
+        public static bool UpdateRelationshipToRequest(string myNickname, string targetNickname)
         {
             try
             {
@@ -578,7 +578,7 @@ namespace Asset.MySql
         /// <param name="myNickname"></param>
         /// <param name="targetNickname"></param>
         /// <returns></returns>
-        public bool UpdateRelationshipToFriend(string myNickname, string targetNickname)
+        public static bool UpdateRelationshipToFriend(string myNickname, string targetNickname)
         {
             try
             {
@@ -599,7 +599,7 @@ namespace Asset.MySql
         /// <param name="myNickname"></param>
         /// <param name="targetNickname"></param>
         /// <returns></returns>
-        public bool UpdateRelationshipToUnFriend(string myNickname, string targetNickname)
+        public static bool UpdateRelationshipToUnFriend(string myNickname, string targetNickname)
         {
             try
             {
@@ -620,19 +620,19 @@ namespace Asset.MySql
             }
         }
 
-        private int UpdateRelationshipToBlockHelper(bool isLeft, int state)
+        private static int UpdateRelationshipToBlockHelper(bool isLeft, int state)
         {
             int shiftBit = isLeft ? _SHIFT_BIT : _RESET_BIT;
             return state ^ (_BLOCK_BIT << shiftBit);
         }
 
-        private int UpdateRelationshipToUnblockHelper(bool isLeft, int state)
+        private static int UpdateRelationshipToUnblockHelper(bool isLeft, int state)
         {
             int shiftBit = isLeft ? _SHIFT_BIT : _RESET_BIT;
             return state | _RESET_BIT << shiftBit;
         }
 
-        private int UpdateRelationshipToRequestHelper(bool isLeft, int state)
+        private static int UpdateRelationshipToRequestHelper(bool isLeft, int state)
         {
             int shiftBit = isLeft ? _SHIFT_BIT : _RESET_BIT;
             return state | _REQUEST_BIT << shiftBit;
