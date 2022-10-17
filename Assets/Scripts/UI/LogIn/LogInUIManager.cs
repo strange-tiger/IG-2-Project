15671a664a952878e9b120f5b2c48f38e4ecd694
strange-tiger <jinolee9525@gmail.com>
2022-10-17 17:41:59 +0900

    [Feat] LogIn씬에 가상키보드 추가
    - InputField가 선택될 때 키보드 활성화 구현
    - VR로 테스트 필요

--- Assets/Scripts/UI/LogIn/LogInUIManager.cs
+++ Assets/Scripts/UI/LogIn/LogInUIManager.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using VRKeys;
 using _UI = Defines.ELogInUIIndex;
 
 public class LogInUIManager : UIManager
 {
+    [SerializeField] Keyboard _keyboard;
+
+    [SerializeField] TMP_InputField[] _inputFields;
+
     private void Awake()
     {
         LoadUI(_UI.LOGIN);
+
+        foreach(TMP_InputField input in _inputFields)
+        {
+            input.onSelect.RemoveListener(ActiveKeyboard);
+            input.onSelect.AddListener(ActiveKeyboard);
+        }
     }
+
+    // _keyboard 활성화 테스트
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Tab))
+    //    {
+    //        _inputFields[0].onSelect.Invoke("aa");
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.Backspace))
+    //    {
+    //        _keyboard.Disable();
+    //    }
+    //}
     
     /// <summary>
     /// ELogInUIIndex를 ui 매개변수로 받아, UIManager.LoadUI에 전달해 
@@ -20,5 +45,19 @@ public class LogInUIManager : UIManager
     public void LoadUI(_UI ui)
     {
         LoadUI((int)ui);
+    }
+
+    private void ActiveKeyboard(string temp)
+    {
+        _keyboard.Enable();
+    }
+
+    private void OnDisable()
+    {
+        foreach (TMP_InputField input in _inputFields)
+        {
+            input.onSelect.RemoveListener(ActiveKeyboard);
+        }
+        _keyboard.Disable();
     }
 }
