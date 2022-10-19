1a8b14c7230ea00996bb5639312debac9b3bb348
csy-59 <csy010509@naver.com>
2022-10-19 10:20:41 +0900

    [Feat] 플레이어 UI Interaction 설정 완료

--- Assets/Jack/Player/NewScripts/PlayerInteraction.cs
+++ Assets/Jack/Player/NewScripts/PlayerInteraction.cs
@@ -1,38 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Photon.Pun;
 
-public class PlayerInteraction : MonoBehaviour
+public class PlayerInteraction : MonoBehaviourPun
 {
     [SerializeField] private PlayerInput _input;
     [SerializeField] private PlayerFocus[] _playerFocus = new PlayerFocus[2];
 
+    [SerializeField] private OVRGazePointer _pointer;
+    private OVRInputModule _eventSystemInputModule;
+
+    private void Awake()
+    {
+        if(photonView.IsMine)
+        {
+            _eventSystemInputModule = FindObjectOfType<OVRInputModule>();
+            _eventSystemInputModule.m_Cursor = _pointer;
+        }
+    }
 
     private void Update()
     {
-        if(_input.IsRay && _input.InputA)
+        if(_input.IsRay)
         {
-            if(_input.PrimaryController == Defines.EPrimaryController.Left)
+            SettingUIInteraction();
+            Interact();
+        }
+        else
+        {
+            _pointer.gameObject.SetActive(false);
+        }
+    }
+
+    private void SettingUIInteraction()
+    {
+        if(_input.PrimaryController == Defines.EPrimaryController.Left)
+        {
+            if(_input.IsLeftRay)
             {
-                if (_input.IsLeftRay)
-                {
-                    CheckAndInteract(_playerFocus[0]);
-                }
-                else
-                {
-                    CheckAndInteract(_playerFocus[1]);
-                }
+                SetPointerTransform(_playerFocus[0]);
             }
             else
             {
-                if (_input.IsRightRay)
-                {
-                    CheckAndInteract(_playerFocus[1]);
-                }
-                else
-                {
-                    CheckAndInteract(_playerFocus[0]);
-                }
+                SetPointerTransform(_playerFocus[1]);
+            }
+        }
+        else
+        {
+            if (_input.IsRightRay)
+            {
+                SetPointerTransform(_playerFocus[1]);
+            }
+            else
+            {
+                SetPointerTransform(_playerFocus[0]);
+            }
+        }
+
+        _pointer.gameObject.SetActive(true);
+    }
+    private void SetPointerTransform(PlayerFocus playerFocus)
+    {
+        _pointer.rayTransform = playerFocus.gameObject.transform;
+        _eventSystemInputModule.rayTransform = playerFocus.gameObject.transform;
+    }
+
+    private void Interact()
+    {
+        if(!_input.InputA)
+        {
+            return;
+        }
+
+        if (_input.PrimaryController == Defines.EPrimaryController.Left)
+        {
+            if (_input.IsLeftRay)
+            {
+                CheckAndInteract(_playerFocus[0]);
+            }
+            else
+            {
+                CheckAndInteract(_playerFocus[1]);
+            }
+        }
+        else
+        {
+            if (_input.IsRightRay)
+            {
+                CheckAndInteract(_playerFocus[1]);
+            }
+            else
+            {
+                CheckAndInteract(_playerFocus[0]);
             }
         }
     }
