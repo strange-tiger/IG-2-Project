cb6e1734598c69f28bafb049a4f250eb33bcbc5f
qwas4689 <qwas4689@gmail.com>
2022-10-20 11:48:04 +0900

    [Feat] Player tag 를 지닌 오브젝트와 접촉 시 MagicWand 스크립트 true
    추후 더 좋은 방법을 찾으면 바꾸겠습니다.

--- Assets/Knight/Scripts_Knight/MiniComeOutMagicWandGame/MagicWand.cs
+++ Assets/Knight/Scripts_Knight/MiniComeOutMagicWandGame/MagicWand.cs
@@ -13,30 +13,23 @@ public class MagicWand : MonoBehaviour
     [Header("쿨타임을 골라주세요")]
     [SerializeField] private Defines.CoolTime _coolTime;
 
+    [SerializeField]
     private ParticleSystem[] _magic;
     private float _currentTime;
     private bool _checkCoolTime;
 
-    void Start()
+    void Awake()
     {
         _magic = new ParticleSystem[transform.childCount];
 
         for (int i = 0; i < transform.childCount; ++i)
         {
-            _probability += _useMagicChance[i];
             _magic[i] = gameObject.transform.GetChild(i).GetComponentInChildren<ParticleSystem>();
-        }
-
-        if (_probability != _totalProbability)
-        {
-            Debug.Log("총 확률이 100이 되지 않습니다. 확인 바랍니다.");
         }
     }
 
     void Update()
     {
-
-
         if (OVRInput.GetDown(OVRInput.Button.Two) || Input.GetKeyDown(KeyCode.K) && !_checkCoolTime)
         {
             int RandomNumber = Random.Range(0, _totalProbability + 1);
