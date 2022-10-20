bcf98aedc92881451567e3a1faf641ed401f23a0
qwas4689 <qwas4689@gmail.com>
2022-10-20 16:06:12 +0900

    [Fix] 마법 사용 후 움직였을 때 마법이 따라오지 않게 수정

--- Assets/Knight/Scripts_Knight/MiniComeOutMagicWandGame/MagicWand.cs
+++ Assets/Knight/Scripts_Knight/MiniComeOutMagicWandGame/MagicWand.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
 
-public class MagicWand : MonoBehaviour
+public class MagicWand : MonoBehaviourPun
 {
     [Header("확률에 해당하는 숫자를 누적시켜 적어주세요")]
     [SerializeField] private int[] _useMagicChance;
 
     private int _totalProbability = 100;
-    private int _probability;
 
     [Header("쿨타임을 골라주세요")]
     [SerializeField] private Defines.CoolTime _coolTime;
 
+    [Header("VRUI의 MagicWandPanel을 넣어주세요")]
     [SerializeField]
+    private GameObject _magicWandPanel;
+
+    private TextMeshProUGUI _magicNameText;
+    private TextMeshProUGUI _magicCoolTimeText;
+
     private ParticleSystem[] _magic;
     private float _currentTime;
     private bool _checkCoolTime;
 
     void Awake()
     {
+        _magicNameText = _magicWandPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        _magicCoolTimeText = _magicWandPanel.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+        _magicWandPanel.SetActive(false);
+
         _magic = new ParticleSystem[transform.childCount];
 
         for (int i = 0; i < transform.childCount; ++i)
@@ -35,6 +48,9 @@ public class MagicWand : MonoBehaviour
             int RandomNumber = Random.Range(0, _totalProbability + 1);
             GetMagic(RandomNumber);
 
+            _magicWandPanel.SetActive(true);
+            _magicNameText.text = gameObject.name;
+
             _checkCoolTime = true;
         }
 
@@ -42,10 +58,16 @@ public class MagicWand : MonoBehaviour
         {
             _currentTime += Time.deltaTime;
 
+            int _coolTimeText = (int)_coolTime;
+
+            _coolTimeText -= (int)_currentTime;
+            _magicCoolTimeText.text = _coolTimeText.ToString();
+
             if (_currentTime > (float)_coolTime)
             {
                 _currentTime -= _currentTime;
                 _checkCoolTime = false;
+                _magicWandPanel.SetActive(false);
             }
         }
     }
--- Assets/Knight/Scripts_Knight/UseParticle.cs
+++ Assets/Knight/Scripts_Knight/UseParticle.cs
@@ -8,13 +8,28 @@ public class UseParticle : MonoBehaviour
     [SerializeField]
     private Defines.EParticleDurationTime _invokeWaitForSeconds;
 
+    private Vector3 _transform;
+
     private void OnEnable()
     {
+        _transform = gameObject.transform.position;
         Invoke("SelfOff", (float)_invokeWaitForSeconds);
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position = _transform;
+        gameObject.transform.rotation = Quaternion.identity;
     }
 
     private void SelfOff()
     {
         gameObject.SetActive(false);
     }
+
+    private void OnDisable()
+    {
+        gameObject.transform.position = GetComponentInParent<Transform>().gameObject.transform.position;
+    }
+
 }
