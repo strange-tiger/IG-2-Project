325f1c4c263d2a18c48791ea0594acb1b5b3108f
yejin9337 <yejin9337@naver.com>
2022-10-20 14:15:55 +0900

    [Fix] 동기화 작업

--- Assets/Ace/Script/PianoButton.cs
+++ Assets/Ace/Script/PianoButton.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Defines;
+using Photon.Pun;
 
-public class PianoButton : MonoBehaviour
+public class PianoButton : MonoBehaviourPun
 {
     [SerializeField]
     private AudioClip _myAudioClip = null;
 
     private AudioSource _audioSource;
     private int _steppedCount = 0;
+    public int SteppedCount
+    {
+        get { return _steppedCount; }
+        set { _steppedCount = value; }
+    }
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
@@ -19,18 +25,31 @@ public class PianoButton : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("´©±¸ µé¾î¿È");
-        ++_steppedCount;
-        if (_steppedCount == 1)
+        if (photonView.IsMine)
         {
-            _audioSource.PlayOneShot(_myAudioClip, _audioSource.volume
-                * PlayerPrefs.GetFloat("EffectVolume"));
-            Debug.Log(_myAudioClip);
+            ++_steppedCount;
+        }
+
+        if(PhotonNetwork.IsMasterClient)
+        {
+            if (SteppedCount == 1)
+            {
+                photonView.RPC("PlayPianoSound", RpcTarget.All);
+            }
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        --_steppedCount;
+        if (photonView.IsMine)
+        {
+            --_steppedCount;
+        }
+    }
+
+    public void PlayPianoSound()
+    {
+        _audioSource.PlayOneShot(_myAudioClip, _audioSource.volume
+                    * PlayerPrefs.GetFloat("EffectVolume"));
     }
 }
