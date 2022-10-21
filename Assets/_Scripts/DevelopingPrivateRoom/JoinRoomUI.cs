f09e0ad2a9544d12a1c4352989fe8e4d7311d47a
strange-tiger <jinolee9525@gmail.com>
2022-10-21 17:47:57 +0900

    [Feat] JoinRoomUI 구현 시작

---
+++ Assets/_Scripts/DevelopingPrivateRoom/JoinRoomUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinRoomUI : MonoBehaviour
+{
+    [Header("Manager")]
+    [SerializeField] PrivateRoomUIManager _uiManager;
+
+    [Header("Button")]
+    [SerializeField] Button _makeRoomButton;
+    [SerializeField] Button _refreshButton;
+    [SerializeField] Button _randomJoinButton;
+
+    [Header("Text")]
+}
