306d6ee4ae6c305c561b4426c743f8ee537019af
csy-59 <csy010509@naver.com>
2022-10-23 16:40:37 +0900

    [Feat] 플레이어 닉네임 전달 적용
    - PlayerNetworking에서 RPC를 사용하여 가져옴

--- Assets/_Scripts/Player/PlayerNetworking.cs
+++ Assets/_Scripts/Player/PlayerNetworking.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class PlayerNetworking : MonoBehaviourPun
 {
     [SerializeField] private Vector3 _ovrCameraPosition;
     [SerializeField] private GameObject _ovrCameraRigPrefab;
+    [SerializeField] private TextMeshProUGUI _nicknameText;
+
+    public string MyNickname { get; private set; }
 
     private void Awake()
     {
@@ -18,5 +22,16 @@ public class PlayerNetworking : MonoBehaviourPun
             PlayerControllerMove playercontroller = gameObject.AddComponent<PlayerControllerMove>();
             playercontroller.CameraRig = cameraRig.GetComponent<OVRCameraRig>();
         }
+        else
+        {
+
+        }
+    }
+
+    [PunRPC]
+    public void SetNickname(string nickname)
+    {
+        MyNickname = nickname;
+        _nicknameText.text = nickname;
     }
 }
