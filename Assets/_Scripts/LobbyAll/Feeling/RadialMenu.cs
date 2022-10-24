cde5e0415d358ec56740b04311819c72648b82c7
Klistas <fukoyjh@gmail.com>
2022-10-24 21:03:20 +0900

    [Fix] 감정표현 동기화
    - Color.a의 값과 Image의 배열을 만들어 배열의 Index를 전달함.
    - newPlayer Prefab에 UI 추가.

--- Assets/_Scripts/LobbyAll/Feeling/RadialMenu.cs
+++ Assets/_Scripts/LobbyAll/Feeling/RadialMenu.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Photon.Pun;
+using System;
+
+
 public class RadialMenu : MonoBehaviourPun, IPunObservable
 {
 
@@ -11,33 +14,37 @@ public class RadialMenu : MonoBehaviourPun, IPunObservable
     [SerializeField] GameObject _radialMenu;
     [SerializeField] Image _cursor;
     [SerializeField] Image _feelingImage;
+    [SerializeField] Image[] _buttonOneImages;
+    [SerializeField] Button[] _buttonOnes;
     public static Button _buttonOne;
     public static Image _buttonOneImage;
-    private static Color _activeColor = new Color(1, 1, 1, 1);
-    private static Color _deactiveColor = new Color(1, 1, 1, 0);
-    private static readonly YieldInstruction _waitSecond = new WaitForSeconds(1f);
 
+    private static readonly YieldInstruction _waitSecond = new WaitForSeconds(1f);
     private Vector2 _cursorInitPosition;
     private float _cursorMovementLimit = 45f;
     private float _cursorSpeed = 100f;
     private float _coolTime = 4f;
+    private float _colorData;
+    private int _buttonIndex;
+
+ 
+
     private void Start()
     {
         _cursorInitPosition = _cursor.rectTransform.localPosition;
-
     }
-
+   
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
-            stream.SendNext(_feelingImage.sprite);
-            stream.SendNext(_feelingImage.color);
+            stream.SendNext(_colorData);
+            stream.SendNext(_buttonIndex);
         }
         else if (stream.IsReading)
         {
-            _feelingImage.sprite = (Sprite)stream.ReceiveNext();
-            _feelingImage.color = (Color)stream.ReceiveNext();
+            _feelingImage.color = new Color(1,1,1,(float)stream.ReceiveNext());
+            _feelingImage.sprite = _buttonOneImages[(int)stream.ReceiveNext()].sprite;
         }
     }
     private void Update()
@@ -68,11 +75,13 @@ public class RadialMenu : MonoBehaviourPun, IPunObservable
         {
             _elapsedTime += Time.deltaTime;
             float animatedFadeAlpha = Mathf.Lerp(startAlpha, endAlpha, Mathf.Clamp01(_elapsedTime / _coolTime));
-            _feelingImage.color = new Color(1, 1, 1, animatedFadeAlpha);
-
+            _colorData = animatedFadeAlpha;
+            _feelingImage.color = new Color(1, 1, 1, _colorData);
             yield return new WaitForSeconds(0.0001f);
         }
-        _feelingImage.color = new Color(1, 1, 1, endAlpha);
+        _colorData = endAlpha;
+        _feelingImage.color = new Color(1, 1, 1, _colorData);
+
         _isFadeRunning = false;
         yield return null;
 
@@ -83,8 +92,19 @@ public class RadialMenu : MonoBehaviourPun, IPunObservable
     {
         if (_buttonOne != null)
         {
-            _feelingImage.color = _activeColor;
-            _feelingImage.sprite = _buttonOneImage.sprite;
+
+            _colorData = 1;
+            _feelingImage.color = new Color(1, 1, 1, _colorData);
+            for(int i = 0; i < _buttonOnes.Length - 1; ++i)
+            {
+                if(_buttonOnes[i].name == _buttonOne.name)
+                {
+                    _buttonIndex = i;
+                }
+            }
+
+            _feelingImage.sprite = _buttonOneImages[_buttonIndex].sprite;
+
             _radialMenu.SetActive(false);
 
 
