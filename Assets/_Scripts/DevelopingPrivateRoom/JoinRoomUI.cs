9ccdba0284dd99451e35aab68994644622a6c7c7
strange-tiger <jinolee9525@gmail.com>
2022-10-24 09:12:16 +0900

    [Fix] 221024 commit #1
    - 221024 시작 시점에서의 오류 수정

--- Assets/_Scripts/DevelopingPrivateRoom/JoinRoomUI.cs
+++ Assets/_Scripts/DevelopingPrivateRoom/JoinRoomUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,4 +15,5 @@ public class JoinRoomUI : MonoBehaviour
     [SerializeField] Button _randomJoinButton;
 
     [Header("Text")]
+    [SerializeField] TextMeshProUGUI[] _roomInfoText;
 }
