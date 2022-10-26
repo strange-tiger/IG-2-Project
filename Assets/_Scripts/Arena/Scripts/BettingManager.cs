c8036be409f86e1cd1ef0ee7d30e8b23dc5d449c
Klistas <fukoyjh@gmail.com>
2022-10-26 12:56:33 +0900

    [Fix] BettingUI 및 BettingManager 수정
    - BettingUI에 베팅 등록 및 취소 추가.
    - ResetBetting() 추가

--- Assets/_Scripts/Arena/Scripts/BettingManager.cs
+++ Assets/_Scripts/Arena/Scripts/BettingManager.cs
@@ -11,14 +11,14 @@ public class BettingManager : MonoBehaviour
     public Dictionary<string, double> BettingFourList = new Dictionary<string, double>();
 
     public double BetAmount;
-    public double[] BetRate;
+    public double[] BetRates;
     public double[] ChampionBetAmounts;
 
-    //분배(DB에 등록), 베팅 시작,종료 알림, 금액 수정시, Dictionary에서 값을 찾아서 반환.
+    //분배(DB에 등록), 베팅 시작, 종료 알림, 금액 수정시, Dictionary에서 값을 찾아서 반환.
 
     private void Awake()
     {
-        ;
+        
     }
 
     private void DistributeGold()
@@ -31,18 +31,25 @@ public class BettingManager : MonoBehaviour
 
     }
 
+  
     private void BettingEnd()
     {
-
+        ResetAllBetting();
     }
 
-    private void FindBetting()
-    {
-
-    }
 
     private void ResetAllBetting()
     {
-        
+        BetAmount = 0;
+        BettingOneList.Clear();
+        BettingTwoList.Clear();
+        BettingThreeList.Clear();
+        BettingFourList.Clear();
+
+        for(int i = 0; i < BetRates.Length; ++i)
+        {
+            BetRates[i] = 0;
+            ChampionBetAmounts[i] = 0;
+        }
     }
 }
--- Assets/_Scripts/Arena/Scripts/BettingUI.cs
+++ Assets/_Scripts/Arena/Scripts/BettingUI.cs
@@ -21,7 +21,9 @@ public class BettingUI : MonoBehaviour
 
     [SerializeField] BettingManager _bettingManager;
 
+    private bool[] _isBetting = new bool[4];
     private string _playerNickname;
+
     private void Start()
     {
         _playerNickname = "dd";
@@ -40,28 +42,129 @@ public class BettingUI : MonoBehaviour
 
         _betChampionFourButton.onClick.RemoveListener(BetChampionFour);
         _betChampionFourButton.onClick.AddListener(BetChampionFour);
+
+        foreach(TMP_InputField inputfield in _betChampionInputField)
+        {
+            inputfield.onSelect.AddListener((string temp) =>
+                {
+                    KeyboardManager.OpenKeyboard(KeyboardManager.EKeyboardLayout.NUMPAD);
+                }
+            );
+        }
+    }
+
+    private bool BettingExist()
+    {
+        for(int i = 0; i < _isBetting.Length; ++i)
+        {
+            if(_isBetting[i])
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private int BetChampion(int index)
     {
-        _bettingManager.BetAmount += double.Parse(_betChampionInputField[index].text);
-        _bettingManager.ChampionBetAmounts[index] += double.Parse(_betChampionInputField[index].text);
-        for(int i = 0; i < _bettingManager.BetRate.Length; ++i)
+        if(BettingExist())
         {
-            _bettingManager.BetRate[i] = (_bettingManager.ChampionBetAmounts[i] / _bettingManager.BetAmount) * 100;
-            _betRateText[i].text = $"{Math.Round(_bettingManager.BetRate[i])}";
+            //ÆÐ³Î ¿Â
+            return -1;
         }
-        return index;
+        else
+        {
+            _isBetting[index] = true;
+            _bettingManager.BetAmount += double.Parse(_betChampionInputField[index].text);
+            _bettingManager.ChampionBetAmounts[index] += double.Parse(_betChampionInputField[index].text);
+            for(int i = 0; i < _bettingManager.BetRates.Length; ++i)
+            {
+                _bettingManager.BetRates[i] = (_bettingManager.ChampionBetAmounts[i] / _bettingManager.BetAmount) * 100;
+                _betRateText[i].text = $"{Math.Round(_bettingManager.BetRates[i])}";
+            }
+
+            return index;
+        }
+    }
+    private void BetCancel(int index, double cancelGold)
+    {
+        
+            _isBetting[index] = false;
+            _bettingManager.BetAmount -= cancelGold;
+            _bettingManager.ChampionBetAmounts[index] -= cancelGold;
+            for (int i = 0; i < _bettingManager.BetRates.Length; ++i)
+            {
+                _bettingManager.BetRates[i] = (_bettingManager.ChampionBetAmounts[i] / _bettingManager.BetAmount) * 100;
+                _betRateText[i].text = $"{Math.Round(_bettingManager.BetRates[i])}";
+            }
+        
+       
+    }
+    private void BetChampionOne()
+    {
+        if(BetChampion(0) != -1)
+        {
+            _bettingManager.BettingOneList.Add(_playerNickname, double.Parse(_betChampionInputField[BetChampion(0)].text));
+        }
     }
 
-    private void BetChampionOne() =>  _bettingManager.BettingOneList.Add(_playerNickname, double.Parse(_betChampionInputField[BetChampion(0)].text));
+    private void BetChampionTwo()
+    {
+        if(BetChampion(1) != -1)
+        {
+            _bettingManager.BettingTwoList.Add(_playerNickname, double.Parse(_betChampionInputField[BetChampion(1)].text));
+        }
+    }
 
-    private void BetChampionTwo() => _bettingManager.BettingTwoList.Add(_playerNickname, double.Parse(_betChampionInputField[BetChampion(1)].text));
+    private void BetChampionThree()
+    {
+        if (BetChampion(2) != -1)
+        {
+            _bettingManager.BettingTwoList.Add(_playerNickname, double.Parse(_betChampionInputField[BetChampion(2)].text));
+        }
+    }
+    private void BetChampionFour()
+    {
+        if (BetChampion(3) != -1)
+        {
+            _bettingManager.BettingTwoList.Add(_playerNickname, double.Parse(_betChampionInputField[BetChampion(3)].text));
+        }
+    }
+    private void BetCancelChampionOne()
+    {
+        if (BettingExist())
+        {
+            BetCancel(0, _bettingManager.BettingOneList[_playerNickname]);
+            _bettingManager.BettingOneList.Remove(_playerNickname);
+        }
+    }
 
-    private void BetChampionThree() => _bettingManager.BettingThreeList.Add(_playerNickname, double.Parse(_betChampionInputField[BetChampion(2)].text));
+    private void BetCancelChampionTwo()
+    {
+        if (BettingExist())
+        {
+            BetCancel(1, _bettingManager.BettingTwoList[_playerNickname]);
+            _bettingManager.BettingOneList.Remove(_playerNickname);
+        }
+    }
 
-    private void BetChampionFour() => _bettingManager.BettingFourList.Add(_playerNickname, double.Parse(_betChampionInputField[BetChampion(3)].text));
+    private void BetCancelChampionThree()
+    {
+        if (BettingExist())
+        {
+            BetCancel(2, _bettingManager.BettingThreeList[_playerNickname]);
+            _bettingManager.BettingOneList.Remove(_playerNickname);
+        }
+    }
 
+    private void BetCancelChampionFour()
+    {
+        if (BettingExist())
+        {
+            BetCancel(3, _bettingManager.BettingFourList[_playerNickname]);
+            _bettingManager.BettingOneList.Remove(_playerNickname);
+        }
+    }
     private void OnDisable()
     {
         _betChampionOneButton.onClick.RemoveListener(BetChampionOne);
