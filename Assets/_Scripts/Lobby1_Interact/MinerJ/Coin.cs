69d5578124d51b9e095c0edcdbbe1ce8254214e6
yejin9337 <yejin9337@naver.com>
2022-10-26 15:03:25 +0900

    [Fix] 코인 획득 시 텍스트 추가

--- Assets/_Scripts/Lobby1_Interact/MinerJ/Coin.cs
+++ Assets/_Scripts/Lobby1_Interact/MinerJ/Coin.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class Coin : MonoBehaviour
 {
     private AudioSource _audioSource;
     private Image _coinImage;
+    private TextMeshProUGUI _coinText;
 
     [SerializeField]
     private Button _miningButton;
@@ -22,6 +24,7 @@ public class Coin : MonoBehaviour
     {
         _audioSource = GetComponent<AudioSource>();
         _coinImage = gameObject.GetComponentInChildren<Image>();
+        _coinText = gameObject.GetComponentInChildren<TextMeshProUGUI>();
     }
 
     public void GetCoin()
@@ -36,14 +39,17 @@ public class Coin : MonoBehaviour
         int _randNum = Random.Range(1,1000);
         if(_randNum == 1000)
         {
+            _coinText.text = "+100";
             _audioSource.PlayOneShot(_clips[2]);
         }
         else if(_randNum >=969)
         {
+            _coinText.text = "+10";
             _audioSource.PlayOneShot(_clips[1]);
         }
         else
         {
+            _coinText.text = "+1";
             _audioSource.PlayOneShot(_clips[0]);
         }
     }
@@ -52,6 +58,7 @@ public class Coin : MonoBehaviour
         gameObject.SetActive(true);
         _elapsedTime = 0;
         _coinImage.color = Color.white;
+        _coinText.color = Color.white;
     }
     IEnumerator GetCoinEffect()
     {
@@ -68,6 +75,7 @@ public class Coin : MonoBehaviour
             else
             {
                 _coinImage.color = new Color( 1, 1, 1, 1f - _elapsedTime / _delay);
+                _coinText.color = new Color(1, 1, 1, 1f - _elapsedTime / _delay);
             }
             yield return null;
         }
