441f9aa1f1c427e0d34954c9899d5a02d89cf6ff
qwas4689 <qwas4689@gmail.com>
2022-10-27 17:46:51 +0900

    [Fix] 무승부 시 멈추게 토너먼트 중지

--- Assets/_Scripts/Arena/Scripts/Tournament/GroupManager.cs
+++ Assets/_Scripts/Arena/Scripts/Tournament/GroupManager.cs
@@ -26,6 +26,8 @@ public class GroupManager : MonoBehaviour
     private int _secondWinnerIndex;
     public int WinnerIndex { get { return _winnerIndex; } private set { _winnerIndex = value; } }
 
+    private bool _isDraw;
+    public bool IsDraw { get { return _isDraw; } private set { _isDraw = value; } }
 
 
     void Start()
@@ -50,64 +52,83 @@ public class GroupManager : MonoBehaviour
 
     void Update()
     {
-        // ÁØ°á½Â 1
-        if ((_member[0].activeSelf == false || _member[1].activeSelf == false) && !_isFirstBattle)
+        if (!_isDraw)
         {
 
-            if (_member[0].activeSelf)
-            {
-                _firstWinnerIndex = 0;
-                _finalBattle[0] = _member[0];
-                _member[0].transform.position = new Vector3(-_setPosition, -2, 0);
-                _member[0].SetActive(false);
-            }
-            else if (_member[1].activeSelf)
-            {
-                _firstWinnerIndex = 1;
-                _finalBattle[0] = _member[1];
-                _member[1].transform.position = new Vector3(-_setPosition, -2, 0);
-                _member[1].SetActive(false);
-            }
 
-            Invoke("SecondBattle", 2f);
-        }
-
-        // ÁØ°á½Â 2
-        if ((_member[2].activeSelf == false || _member[3].activeSelf == false) && _member[0].activeSelf == false && _member[1].activeSelf == false && _isSecondBattle)
-        {
-
-            if (_member[2].activeSelf)
+            // ÁØ°á½Â 1
+            if ((_member[0].activeSelf == false || _member[1].activeSelf == false) && !_isFirstBattle)
             {
-                _secondWinnerIndex = 2;
-                _finalBattle[1] = _member[2];
-                _member[2].transform.position = new Vector3(_setPosition, -2, 0);
-                _member[2].SetActive(false);
-            }
-            else if (_member[3].activeSelf)
-            {
-                _secondWinnerIndex = 3;
-                _finalBattle[1] = _member[3];
-                _member[3].transform.position = new Vector3(_setPosition, -2, 0);
-                _member[3].SetActive(false);
+                if (_member[0].activeSelf == false && _member[1].activeSelf == false)
+                {
+                    _isDraw = true;
+                }
+
+                if (_member[0].activeSelf)
+                {
+                    _firstWinnerIndex = 0;
+                    _finalBattle[0] = _member[0];
+                    _member[0].transform.position = new Vector3(-_setPosition, -2, 0);
+                    _member[0].SetActive(false);
+                }
+
+                else if (_member[1].activeSelf)
+                {
+                    _firstWinnerIndex = 1;
+                    _finalBattle[0] = _member[1];
+                    _member[1].transform.position = new Vector3(-_setPosition, -2, 0);
+                    _member[1].SetActive(false);
+                }
+
+                Invoke("SecondBattle", 2f);
             }
 
-            Invoke("FinalBattle", 2f);
+            // ÁØ°á½Â 2
+            if ((_member[2].activeSelf == false || _member[3].activeSelf == false) && _member[0].activeSelf == false && _member[1].activeSelf == false && _isSecondBattle)
+            {
+                if (_member[2].activeSelf == false && _member[3].activeSelf == false)
+                {
+                    _isDraw = true;
+                }
 
+                if (_member[2].activeSelf)
+                {
+                    _secondWinnerIndex = 2;
+                    _finalBattle[1] = _member[2];
+                    _member[2].transform.position = new Vector3(_setPosition, -2, 0);
+                    _member[2].SetActive(false);
+                }
+
+                else if (_member[3].activeSelf)
+                {
+                    _secondWinnerIndex = 3;
+                    _finalBattle[1] = _member[3];
+                    _member[3].transform.position = new Vector3(_setPosition, -2, 0);
+                    _member[3].SetActive(false);
+                }
+
+                Invoke("FinalBattle", 2f);
+
+            }
+
+            if (_member[0].activeSelf == false && _member[1].activeSelf == false && _member[2].activeSelf == false && _member[3].activeSelf == false && _isFinelBattle == true)
+            {
+                _finalBattle[0].SetActive(true);
+                _finalBattle[1].SetActive(true);
+
+                _isWinnerIndex = true;
+            }
+
+            if (_finalBattle[0].activeSelf == false && _finalBattle[0].activeSelf == false)
+            {
+                _isDraw = true;
+            }
+
+            if (_isWinnerIndex)
+            {
+                SendWinnerIndex();
+            }
         }
-
-        if (_member[0].activeSelf == false && _member[1].activeSelf == false && _member[2].activeSelf == false && _member[3].activeSelf == false && _isFinelBattle == true)
-        {
-            _finalBattle[0].SetActive(true);
-            _finalBattle[1].SetActive(true);
-
-            _isWinnerIndex = true;
-        }
-
-        if (_isWinnerIndex)
-        {
-            SendWinnerIndex();
-        }
-
     }
 
     // Á×Àº AI
