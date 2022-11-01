a871c382326ad2accc918b5264e3bd4fea0a1a21
csy-59 <csy010509@naver.com>
2022-11-01 14:41:17 +0900

    [Feat] 회전초 프리팹 완성
    - 일정 범위 안에 플레이어가 들어오면 아웃라인 보여주기
    - 플레이어를 향해 UI 보여주기

--- Assets/_Scripts/DevelopingFindingGold/TumbleweedMovement.cs
+++ Assets/_Scripts/DevelopingFindingGold/TumbleweedMovement.cs
@@ -1,17 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EPOOutline;
 
 public class TumbleweedMovement : MonoBehaviour
 {
     [SerializeField] private float _bounceForce = 2f;
     private Rigidbody _rigidbody;
 
+    [SerializeField] private Color _outlineColor = new Color(1f, 0.9f, 0.01f);
+    private Outlinable _outline;
+
+    [SerializeField] private Transform _slider;
+    private Transform _player;
+    private bool _isTherePlayer;
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.AddForce(transform.forward * _bounceForce, ForceMode.Impulse);
+
+        _outline = GetComponent<Outlinable>();
+        _outline.AddAllChildRenderersToRenderingList();
+        _outline.OutlineParameters.Color = _outlineColor;
+        _outline.enabled = false;
+
         Destroy(gameObject, 20f);
     }
 
+    private void FixedUpdate()
+    {
+        _slider.rotation = Quaternion.Euler(0f, _slider.rotation.y, _slider.rotation.z);
+
+        if(_isTherePlayer)
+        {
+            _slider.transform.LookAt(_player);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            _outline.enabled = true;
+            _player = other.transform;
+            _isTherePlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            _outline.enabled = false;
+            _isTherePlayer = false;
+            _slider.gameObject.SetActive(false);
+        }
+    }
 }
