11107d802c6846fa081a60f3cbd82a81873dc3e1
yejin9337 <yejin9337@naver.com>
2022-11-01 16:32:19 +0900

    [Feat] 선빵필승 구현중
    1. 플레이어와 충돌 시 병 깨짐 구현 중
    2. 플레이어와 충돌 시 맞은 대상 스턴 구현 중

--- Assets/_Scripts/Lobby2_Interact/FirstMoveAttack/FirstMoveAttackObj.cs
+++ Assets/_Scripts/Lobby2_Interact/FirstMoveAttack/FirstMoveAttackObj.cs
@@ -1,34 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class FirstMoveAttackObj : MonoBehaviour
+public class FirstMoveAttackObj : MonoBehaviourPun
 {
-
-    void Start()
+    private Vector3 _objSpawnPos;
+    private void Awake()
     {
-        
+        _objSpawnPos = transform.position;
     }
 
 
-    void Update()
+    private void OnTriggerEnter(Collider other) // 상대 머리 가격한 경우
     {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
+        if(other.CompareTag("Player")) // 근데 이러면 그냥 내 손만 닿아도 깨질 것 같은데? 오브젝트 입장에서 나 자신을 어떻게 제외시켜야할까?
         {
-            
+            SoundManager.Instance.PlaySFXSound("쨍그랑!/"); //해당 사운드
+            PhotonNetwork.Destroy(gameObject);
+            Invoke("Respawn", 2f);
+            Stun();
         }
     }
 
-    public void OnTriggerExit(Collider other)
+    public void OnTriggerExit(Collider other) // 놓쳐버린 경우
     {
-        if(other.gameObject.tag == "Player")
+        if (other.CompareTag("Player"))
         {
-
+            PhotonNetwork.Destroy(gameObject);
+            Respawn();
         }
     }
+
+    public void Stun()
+    {
+        PlayerControlManager.Instance.IsMoveable = false;
+        PlayerControlManager.Instance.IsRayable = false;
+    }
+
+    public void Respawn()
+    {
+        PlayerControlManager.Instance.IsMoveable = true;
+        PlayerControlManager.Instance.IsRayable = true;
+        PhotonNetwork.Instantiate("bottle", _objSpawnPos, Quaternion.identity);
+    }
+
 }
