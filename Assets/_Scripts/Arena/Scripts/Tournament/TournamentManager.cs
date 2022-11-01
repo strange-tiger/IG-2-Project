c8b7e539a7259aa44a5a70588cfdb91cfed73598
qwas4689 <qwas4689@gmail.com>
2022-11-01 15:55:30 +0900

    [Add] Chaos 그룹 에셋 추가

--- Assets/_Scripts/Arena/Scripts/Tournament/TournamentManager.cs
+++ Assets/_Scripts/Arena/Scripts/Tournament/TournamentManager.cs
@@ -26,7 +26,7 @@ public class TournamentManager : MonoBehaviour
     {
         _selectGroup = Random.Range(0, _groups.Length);
 
-        _selectGroup = 1;
+        _selectGroup = 2;
 
         _groups[_selectGroup].SetActive(true);
     }
