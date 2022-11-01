17935ac0a6c0e964ba99a3bed96e34d82f2316f2
strange-tiger <jinolee9525@gmail.com>
2022-11-01 15:24:55 +0900

    [Feat] 사설 공간 오브젝트 등 Focusable Object 추가
    - Legal_pad
    - Pencil
    - Dice
    - Wood

--- Assets/_Scripts/Lobby2_Interact/WesternCampfire/Wood.cs
+++ Assets/_Scripts/Lobby2_Interact/WesternCampfire/Wood.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Wood : MonoBehaviour
+public class Wood : MonoBehaviourPun
 {
     
 }
