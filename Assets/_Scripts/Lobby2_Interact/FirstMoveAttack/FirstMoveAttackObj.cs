198fea1928b15f3b1524039366ad30008cd63ba3
yejin9337 <yejin9337@naver.com>
2022-11-02 17:33:30 +0900

    [Feat] 선빵필승 구현중
    1. 플레이어에게 부딪혔을때 병 파괴
    2. 플레이어 스턴 (화면 꺼짐, 이동 불가) / 이동 불가는 구현, 화면 꺼짐 구현 중
    3. 병 재 생성
    그랩 상태임을 가져와 플레이어와 연결하는 작업 필요

--- Assets/_Scripts/Lobby2_Interact/FirstMoveAttack/FirstMoveAttackObj.cs
+++ Assets/_Scripts/Lobby2_Interact/FirstMoveAttack/FirstMoveAttackObj.cs
@@ -6,28 +6,13 @@ using Photon.Pun;
 public class FirstMoveAttackObj : MonoBehaviourPun
 {
     private Vector3 _objSpawnPos;
-    private float _fadeTime = 2f;
+
     private void Awake()
     {
         _objSpawnPos = transform.position;
     }
 
-
-    private void OnTriggerEnter(Collider other) // 상대를 가격한 경우
-    {
-        if(other.CompareTag("Player")) 
-            // 근데 이러면 그냥 내 손만 닿아도 깨질 것 같은데? 오브젝트 입장에서 나 자신을 어떻게 제외시켜야할까? 예외처리 필요함
-        {
-            SoundManager.Instance.PlaySFXSound("쨍그랑!"); //해당 사운드 //근데 이것도 동기화 해야 할 듯
-            PhotonNetwork.Destroy(gameObject);
-            // other의 화면 까맣게 하기
-            Stun();
-            StartCoroutine(FadeIn(0, 1));
-            Invoke("Respawn", _fadeTime);
-        }
-    }
-
-    public void OnTriggerExit(Collider other) // 놓쳐버린 경우
+    public void OnTriggerExit(Collider other) // 놓쳐버린 경우, 조금 더 디테일할 필요 있음
     {
         if (other.CompareTag("Player"))
         {
@@ -36,31 +21,15 @@ public class FirstMoveAttackObj : MonoBehaviourPun
         }
     }
 
-    public void Stun()
+    public void Crack()
     {
-        PlayerControlManager.Instance.IsMoveable = false;
-        PlayerControlManager.Instance.IsRayable = false;
+        //SoundManager.Instance.PlaySFXSound("쨍그랑!"); //해당 사운드 //근데 이것도 동기화 해야 할 듯
+        Invoke("Respawn", 2f); // fade-in delay 타임 넣을 것
+        PhotonNetwork.Destroy(gameObject);
     }
 
     public void Respawn()
     {
-        PhotonNetwork.Instantiate("bottle", _objSpawnPos, Quaternion.identity);
-    }
-
-    IEnumerator FadeIn(float startAlpha, float endAlpha)
-    {
-        float elapsedTime = 0;
-
-        while (elapsedTime < _fadeTime)
-        {
-            elapsedTime += Time.deltaTime;
-            // 상대방 화면 = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / _fadeTime);
-        }
-
-        // 상대방 화면 = Mathf.Lerp(startAlpha, endAlpha, 1);
-        PlayerControlManager.Instance.IsMoveable = true;
-        PlayerControlManager.Instance.IsRayable = true;
-
-        yield return null;
+        PhotonNetwork.Instantiate(gameObject.name, _objSpawnPos, Quaternion.identity);
     }
 }
