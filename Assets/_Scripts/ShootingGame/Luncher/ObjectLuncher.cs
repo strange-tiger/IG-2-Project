3a0f4cadb385052c1a69d444f835425924dbccc0
csy-59 <csy010509@naver.com>
2022-11-05 14:04:00 +0900

    [Fix] 수치 오류 수정
    - ObjectLuncher의 X, Y 기울기를 반대로 줌

--- Assets/_Scripts/ShootingGame/Luncher/ObjectLuncher.cs
+++ Assets/_Scripts/ShootingGame/Luncher/ObjectLuncher.cs
@@ -8,8 +8,8 @@ public class ObjectLuncher : MonoBehaviour
     [SerializeField] private float _minXDegree = -15f;
     [SerializeField] private float _maxXDegree = 30f;
 
-    [SerializeField] private float _minZDegree = -10f;
-    [SerializeField] private float _maxZDegree = 10f;
+    [SerializeField] private float _minYDegree = -10f;
+    [SerializeField] private float _maxYDegree = 10f;
 
     [SerializeField] private Vector3 _originalDegree = new Vector3(0f, 0f, 0f);
 
@@ -32,7 +32,7 @@ public class ObjectLuncher : MonoBehaviour
         }
 
         float xDegree = Random.Range(_minXDegree, _maxXDegree);
-        float zDegree = Random.Range(_minZDegree, _maxZDegree);
+        float zDegree = Random.Range(_minYDegree, _maxYDegree);
 
         transform.rotation = Quaternion.Euler(_originalDegree);
         transform.rotation = Quaternion.Euler(xDegree + _originalDegree.x, zDegree + _originalDegree.y, _originalDegree.z);
