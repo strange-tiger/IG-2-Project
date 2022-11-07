7f69446c50130b1a6fa55f37b6c6d4920884d553
csy-59 <csy010509@naver.com>
2022-11-07 09:41:02 +0900

    [Fix] 한번에 발사하는 오브젝트 수 변경함

--- Assets/_Scripts/ShootingGame/Luncher/ObjectLuncher.cs
+++ Assets/_Scripts/ShootingGame/Luncher/ObjectLuncher.cs
@@ -11,6 +11,8 @@ public class ObjectLuncher : MonoBehaviour
     [SerializeField] private float _minYDegree = -10f;
     [SerializeField] private float _maxYDegree = 10f;
 
+    [SerializeField] private int _maxLunchCount = 3;
+
     [SerializeField] private Vector3 _originalDegree = new Vector3(0f, 0f, 0f);
 
     [SerializeField] private LuncherManager.ELuncherId _luncherId;
@@ -31,12 +33,15 @@ public class ObjectLuncher : MonoBehaviour
             return;
         }
 
-        float xDegree = Random.Range(_minXDegree, _maxXDegree);
-        float zDegree = Random.Range(_minYDegree, _maxYDegree);
+        for(int i = 0; i<_maxLunchCount; ++i)
+        {
+            float xDegree = Random.Range(_minXDegree, _maxXDegree);
+            float zDegree = Random.Range(_minYDegree, _maxYDegree);
 
-        transform.rotation = Quaternion.Euler(_originalDegree);
-        transform.rotation = Quaternion.Euler(xDegree + _originalDegree.x, zDegree + _originalDegree.y, _originalDegree.z);
+            transform.rotation = Quaternion.Euler(_originalDegree);
+            transform.rotation = Quaternion.Euler(xDegree + _originalDegree.x, zDegree + _originalDegree.y, _originalDegree.z);
 
-        Instantiate(prefab, _lunchPointTransform.position, Quaternion.Euler(xDegree + _originalDegree.x, zDegree + _originalDegree.y, _originalDegree.z));
+            Instantiate(prefab, _lunchPointTransform.position, Quaternion.Euler(xDegree + _originalDegree.x, zDegree + _originalDegree.y, _originalDegree.z));
+        }
     }
 }
