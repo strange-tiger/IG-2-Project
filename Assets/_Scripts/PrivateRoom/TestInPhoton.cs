784b62a6e554ae94d8d1dc9444c8cca34badf818
qwas4689 <qwas4689@gmail.com>
2022-11-07 21:18:24 +0900

    [Fix] 동기화 중간 저장

--- Assets/_Scripts/PrivateRoom/TestInPhoton.cs
+++ Assets/_Scripts/PrivateRoom/TestInPhoton.cs
@@ -6,23 +6,23 @@ using Photon.Realtime;
 
 public class TestInPhoton : MonoBehaviourPunCallbacks
 {
+    [SerializeField]
+    private Transform ffff;
+
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings();
-        Debug.Log("접속중");
     }
 
     private void OnDisconnectedFromMasterServer()
     {
         PhotonNetwork.ConnectUsingSettings();
-        Debug.Log("접속 실패 다시 접속 중");
     }
 
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
         PhotonNetwork.JoinLobby();
-        Debug.Log("마스터접속 완료 로비 접속 중");
     }
 
     private static readonly RoomOptions RandomRoomOptions = new RoomOptions()
@@ -33,7 +33,6 @@ public class TestInPhoton : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         base.OnJoinedLobby();
-        Debug.Log("로비 접속 완료");
 
         PhotonNetwork.JoinOrCreateRoom("aa", RandomRoomOptions, TypedLobby.Default);
     }
@@ -41,8 +40,6 @@ public class TestInPhoton : MonoBehaviourPunCallbacks
     public override void OnCreatedRoom()
     {
         base.OnCreatedRoom();
-        Debug.Log($"생성한 방 이름 : {PhotonNetwork.CurrentRoom.Name}");
-        Debug.Log("방 생성");
 
         PhotonNetwork.LoadLevel("ArenaRoom");
     }
@@ -50,8 +47,6 @@ public class TestInPhoton : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        Debug.Log($"참가한 방 이름 : {PhotonNetwork.CurrentRoom.Name}");
-        Debug.Log("룸 참가");
-        PhotonNetwork.Instantiate("NewPlayer", Vector3.zero, Quaternion.identity);
+        PhotonNetwork.Instantiate("NewPlayer", ffff.position, Quaternion.identity);
     }
 }
