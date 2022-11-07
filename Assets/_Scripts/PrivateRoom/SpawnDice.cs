9172a2354d88abffbe98a65ee33969c355fa127a
Klistas <fukoyjh@gmail.com>
2022-11-07 11:45:41 +0900

    [DB] GetPetInventory 구현.
    - 펫의 상태, 레벨, 경험치, 착용 에셋, 사이즈 등을 List로 가져오는 GetPetInventory 구현

--- Assets/_Scripts/PrivateRoom/SpawnDice.cs
+++ Assets/_Scripts/PrivateRoom/SpawnDice.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class SpawnDice : MonoBehaviourPun, IPunObservable
+public class SpawnDice : MonoBehaviourPun
 {
     [SerializeField] GameObject _dice;
 
