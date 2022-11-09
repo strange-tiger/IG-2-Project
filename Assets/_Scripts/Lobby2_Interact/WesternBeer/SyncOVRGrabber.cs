95daedc45969e048540d93adee81c62f91daa851
csy-59 <103083746+yejin9337@users.noreply.github.com>
2022-11-09 10:52:11 +0900

    [Chore] 테스트용 디버그

--- Assets/_Scripts/Lobby2_Interact/WesternBeer/SyncOVRGrabber.cs
+++ Assets/_Scripts/Lobby2_Interact/WesternBeer/SyncOVRGrabber.cs
@@ -324,6 +324,7 @@ public class SyncOVRGrabber : MonoBehaviourPun
             return;
         }
 
+        Debug.Log($"클라이언트가 물체 잡음\n pos :{pos}, quaternion : {rot}, bool : {forceTeleport}");
         Rigidbody grabbedRigidbody = m_grabbedObj.grabbedRigidbody;
         Vector3 grabbablePosition = pos + rot * m_grabbedObjectPosOff;
         Quaternion grabbableRotation = rot * m_grabbedObjectRotOff;
