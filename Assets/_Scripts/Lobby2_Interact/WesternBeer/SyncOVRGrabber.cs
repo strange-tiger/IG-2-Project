26e46a43fe61989d273db80571ffbe2a15f0cefc
Klistas <fukoyjh@gmail.com>
2022-11-10 17:10:18 +0900

    [Fix] SyncOVRGrabber FIx

--- Assets/_Scripts/Lobby2_Interact/WesternBeer/SyncOVRGrabber.cs
+++ Assets/_Scripts/Lobby2_Interact/WesternBeer/SyncOVRGrabber.cs
@@ -351,7 +351,9 @@ public class SyncOVRGrabber : MonoBehaviourPun
                 if (!m_parentHeldObject)
                 {
                     _grabbedHandCollider.isTrigger = false;
-                    _grabbedHandCollider = null;
+                    //_grabbedHandCollider = null;
+                    GrabEnd();
+
                 }
             }
         }
