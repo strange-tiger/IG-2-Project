911ed6a97d034815d4e12f2d9ccaaf24f3257b5f
qwas4689 <qwas4689@gmail.com>
2022-11-10 12:38:30 +0900

    [Chore] 깃 충돌 처리 중

--- Assets/_Scripts/PrivateRoom/TestInPhoton.cs
+++ Assets/_Scripts/PrivateRoom/TestInPhoton.cs
@@ -1,53 +1,60 @@
+//#define debugMaster
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
-
+using Asset.MySql;
 public class TestInPhoton : MonoBehaviourPunCallbacks
 {
-
+    private const string ROOM_NAME = "PetTest";
 
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings();
-        Debug.Log("서버에 접속중입니다");
     }
 
-    private void OnDisconnectedFromMasterServer()
+    public override void OnDisconnected(DisconnectCause cause)
     {
+        base.OnDisconnected(cause);
+
         PhotonNetwork.ConnectUsingSettings();
-        Debug.Log("서버에 접속실패해서 다시 시작중입니다");
+        Debug.Log("서버 연결 실패");
     }
 
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
-        PhotonNetwork.JoinLobby();
-    }
 
-    private static readonly RoomOptions RandomRoomOptions = new RoomOptions()
-    {
-        MaxPlayers = 10
-    };
+        PhotonNetwork.JoinLobby();
+        Debug.Log("됐나?");
+    }
 
     public override void OnJoinedLobby()
     {
         base.OnJoinedLobby();
 
-        PhotonNetwork.JoinOrCreateRoom("aa", RandomRoomOptions, TypedLobby.Default);
+        Debug.Log("됐지?");
+#if debugMaster
+        while (!PhotonNetwork.CreateRoom(ROOM_NAME)) ;
+#else
+        while (!PhotonNetwork.JoinRoom(ROOM_NAME)) ;
+#endif
     }
 
     public override void OnCreatedRoom()
     {
         base.OnCreatedRoom();
 
-        PhotonNetwork.LoadLevel("ArenaRoom");
+        Debug.Log("됐구나?");
     }
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
+        Debug.Log("된거구나?");
+        MySqlSetting.Init();
+
         PhotonNetwork.Instantiate("NewPlayer", Vector3.zero, Quaternion.identity);
     }
 }
