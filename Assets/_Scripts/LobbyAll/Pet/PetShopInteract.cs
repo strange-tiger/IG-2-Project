fe6d04315c0f6ae0e03cf5aafea1a90fa3e66db6
Klistas <fukoyjh@gmail.com>
2022-11-11 09:38:42 +0900

    [Feat] Pet Npc 추가

---
+++ Assets/_Scripts/LobbyAll/Pet/PetShopInteract.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetShopInteract : InteracterableObject
+{
+    public override void Interact()
+    {
+        base.Interact();
+
+    }
+}
