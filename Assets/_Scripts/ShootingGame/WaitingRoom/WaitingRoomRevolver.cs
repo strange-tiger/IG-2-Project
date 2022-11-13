d3b0e9fbae4f3d41190bb49bdc72556f62e12f40
yejin9337 <103083746+yejin9337@users.noreply.github.com>
2022-11-13 22:15:29 +0900

    [Chore]Revolver 구현중

--- Assets/_Scripts/ShootingGame/WaitingRoom/WaitingRoomRevolver.cs
+++ Assets/_Scripts/ShootingGame/WaitingRoom/WaitingRoomRevolver.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WaitingRoomRevolver : MonoBehaviour
 {
     private bool _isGrabbed = false;
+    private bool _isReloading = false;
+    private const int _MAX_BULLET_COUNT = 6;
+    private int _bulletCount = 0;
+    private int BulletCount
+    {
+        get { return _bulletCount; }
+        set 
+        {
+            _bulletCount = value;
+            _bulletCountText.text = _bulletCount.ToString();
+        }
+    }
 
-    private ParticleSystem[] _shootEffects = new ParticleSystem[2];
+    // UI및 효과
+    [SerializeField]
+    private TextMeshProUGUI _bulletCountText;
+
+    [SerializeField] private AudioClip _shotAudioClip;
+    [SerializeField] private AudioClip _reloadAudioClip;
     private AudioSource _audioSource;
 
     private void Awake()
     {
+        BulletCount = _MAX_BULLET_COUNT;
+
         _audioSource = GetComponent<AudioSource>();
-        _shootEffects = GetComponentsInChildren<ParticleSystem>();
     }
 
     void Update()
@@ -22,7 +41,13 @@ public class WaitingRoomRevolver : MonoBehaviour
             return;
         }
 
+        //if (!_isShootable)
+        //{
+        //    return;
+        //}
 
+        //Reload();
+        //Shot();
     }
 
     public void OnGrabBegin()
@@ -34,5 +59,24 @@ public class WaitingRoomRevolver : MonoBehaviour
     {
         _isGrabbed = false;
     }
-    
+
+    private void Reload()
+    {
+        // 다시 위로 향하면 장전 종료
+        if (_isReloading)
+        {
+            if (Vector3.Dot(transform.forward, Vector3.down) <= 0.5f)
+            {
+                _isReloading = false;
+            }
+        }
+        // 아래를 보고 있다면 장전
+        else if (Vector3.Dot(transform.forward, Vector3.down) >= 0.8f)
+        {
+            Debug.Log("[Gun] Reload");
+            _bulletCount = _MAX_BULLET_COUNT;
+            _audioSource.PlayOneShot(_reloadAudioClip);
+            _isReloading = true;
+        }
+    }
 }
