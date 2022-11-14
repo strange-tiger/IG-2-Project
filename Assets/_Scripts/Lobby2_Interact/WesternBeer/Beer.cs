a47ed2ceffdda25a3b3ca988a9276f200dd05d9e
Klistas <fukoyjh@gmail.com>
2022-11-14 12:33:44 +0900

    [Fix] Beer 버그 수정 & Grabbable Rotation 수정

--- Assets/_Scripts/Lobby2_Interact/WesternBeer/Beer.cs
+++ Assets/_Scripts/Lobby2_Interact/WesternBeer/Beer.cs
@@ -11,7 +11,7 @@ public class Beer : InteracterableObject, IPunObservable
 
     [SerializeField] GameObject _fullBeer;
     [SerializeField] AudioClip _drinkSound;
-    [SerializeField] BoxCollider _boxCollider;
+    [SerializeField] BoxCollider _grabCollider;
     private BoxCollider _beerCollider;
     private Vector3 _initBeerPosition;
     private YieldInstruction _regenerateTime = new WaitForSeconds(30f);
@@ -23,13 +23,13 @@ public class Beer : InteracterableObject, IPunObservable
         {
             stream.SendNext(_fullBeer.activeSelf);
             stream.SendNext(_beerCollider.enabled);
-            stream.SendNext(_boxCollider.enabled);
+            stream.SendNext(_grabCollider.enabled);
         }
         else if (stream.IsReading)
         {
             _fullBeer.SetActive((bool)stream.ReceiveNext());
             _beerCollider.enabled = (bool)stream.ReceiveNext();
-            _boxCollider.enabled = (bool)stream.ReceiveNext();
+            _grabCollider.enabled = (bool)stream.ReceiveNext();
         }
     }
 
@@ -37,8 +37,8 @@ public class Beer : InteracterableObject, IPunObservable
 
     public void Start()
     {
-        _initBeerPosition = transform.localPosition;
-        _boxCollider = GetComponent<BoxCollider>();
+        _initBeerPosition = transform.position;
+        _grabCollider = GetComponent<BoxCollider>();
         _audioSource = GetComponent<AudioSource>();
     }
 
@@ -53,7 +53,7 @@ public class Beer : InteracterableObject, IPunObservable
         _audioSource.PlayOneShot(_drinkSound);
         _fullBeer.SetActive(false);
         _beerCollider.enabled = false;
-        _boxCollider.enabled = false;
+        _grabCollider.enabled = false;
         StartCoroutine(ReGenerateBeer());
     }
 
@@ -69,7 +69,7 @@ public class Beer : InteracterableObject, IPunObservable
 
         _beerCollider.enabled = true;
 
-        _boxCollider.enabled = true;
+        _grabCollider.enabled = true;
 
         yield return null;
     }
