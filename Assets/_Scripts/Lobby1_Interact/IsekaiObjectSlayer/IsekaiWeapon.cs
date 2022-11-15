87a2bf66f59bb4d3cee127a25fa981ca28c0c3ac
Klistas <fukoyjh@gmail.com>
2022-11-15 16:26:56 +0900

    [Feat] 무기 보관대 무기 생성(활성화) 기능 구현
    - 오브젝트 풀링

--- Assets/_Scripts/Lobby1_Interact/IsekaiObjectSlayer/IsekaiWeapon.cs
+++ Assets/_Scripts/Lobby1_Interact/IsekaiObjectSlayer/IsekaiWeapon.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
-public class IsekaiWeapon : MonoBehaviourPun
+public class IsekaiWeapon : MonoBehaviour
 {
-    [Header("AttackPoints")]
-    [SerializeField] Collider[] _attackPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
---
+++ Assets/_Scripts/Lobby1_Interact/IsekaiObjectSlayer/WeaponRack.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class WeaponRack : MonoBehaviourPun
+{
+    [Header("Weapons")]
+    [SerializeField] Transform[] _weapons;
+
+    private Vector3[] _initWeaponPositions;
+    private int[] _weaponIndexGroup;
+    private int[] _weaponMaxIndexGroup;
+
+    private void Awake()
+    {
+        if (_weapons.Length == 0)
+        {
+            _weapons = new Transform[transform.childCount - 1];
+
+            for (int i = 0; i < transform.childCount - 1; ++i)
+            {
+                _weapons[i] = transform.GetChild(i + 1);
+            }
+        }
+
+        int weaponNum = _weapons.Length;
+        _initWeaponPositions = new Vector3[weaponNum];
+        _weaponIndexGroup = new int[weaponNum];
+        _weaponMaxIndexGroup = new int[weaponNum];
+        for (int i = 0; i < weaponNum; ++i)
+        {
+            _initWeaponPositions[i] = _weapons[i].position;
+            _weaponIndexGroup[i] = 0;
+            _weaponMaxIndexGroup[i] = _weapons[i].childCount;
+        }
+        
+        foreach (Transform weapons in _weapons)
+        {
+            weapons.GetChild(0).gameObject.SetActive(true);
+
+            for (int i = 1; i < weapons.childCount; ++i)
+            {
+                weapons.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("IsekaiWeapon"))
+        {
+            return;
+        }
+
+        int currentIndex = 0;
+        for (int i = 0; i < _weapons.Length; ++i)
+        {
+            if (other.transform.IsChildOf(_weapons[i]))
+            {
+                ++_weaponIndexGroup[i];
+                currentIndex = i;
+
+                break;
+            }
+        }
+
+        if (_weaponIndexGroup[currentIndex] >= _weaponMaxIndexGroup[currentIndex])
+        {
+            _weaponIndexGroup[currentIndex] = 0;
+        }
+
+        _weapons[currentIndex].GetChild(_weaponIndexGroup[currentIndex]).gameObject.SetActive(true);
+    }
+}
