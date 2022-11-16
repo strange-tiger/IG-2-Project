0454d428b83358433eee0da2b14733f1d29e7209
qwas4689 <qwas4689@gmail.com>
2022-11-16 12:48:33 +0900

    [Fix] 컨틀롤러 스왑 스크롤 스크립트 추가

--- Assets/_Scripts/UI/ControllerPanel/ControlScrollUI.cs
+++ Assets/_Scripts/UI/ControllerPanel/ControlScrollUI.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ControlScrollUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private TextMeshProUGUI voiceUIText;
 
-    // Update is called once per frame
-    void Update()
+    public void TextOutput(Defines.EVoiceType voiceType)
     {
-        
+        voiceUIText.text = voiceType.ToString();
     }
 }
