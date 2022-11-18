ccae23d8381f402ae5c47b85e8c1293da82819d2
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-11-18 11:19:21 +0900

    [Feat] 골드 관련 UI 구성 및 표시 기능 구현 중
    - 골드 관련 UI 구성
      - GetGold
    - 골드 표시 기능 구현 중
      - SummonCircle.cs/GetGold&ShowGoldUI

--- Assets/_Scripts/Lobby1_Interact/IsekaiObjectSlayer/SummonCircle.cs
+++ Assets/_Scripts/Lobby1_Interact/IsekaiObjectSlayer/SummonCircle.cs
@@ -6,12 +6,18 @@ using Photon.Pun;
 
 public class SummonCircle : MonoBehaviourPun
 {
+    [Header("Isekai Objects")]
     [SerializeField] IsekaiObject[] _objects;
+
+    [Header("UI")]
+    [SerializeField] GameObject _goldUI;
 
     private static readonly WaitForSeconds SPAWN_DELAY = new WaitForSeconds(1f);
     private static readonly Vector3 FLOAT_POSITION = new Vector3(0f, 1.2f, 0f);
     private static readonly Vector3 WAIT_POSITION = new Vector3(0f, -0.5f, 0f);
     private const float RISE_TIME = 1f;
+
+    private Vector3 _playerPosition = new Vector3();
 
     private void OnEnable()
     {
@@ -24,7 +30,9 @@ public class SummonCircle : MonoBehaviourPun
             obj.gameObject.SetActive(false);
         }
 
-        SpawnHelper();
+        SpawnHelper(_playerPosition);
+
+        _goldUI.SetActive(false);
 #else
         foreach (IsekaiObject obj in _objects)
         {
@@ -33,14 +41,16 @@ public class SummonCircle : MonoBehaviourPun
 
             obj.gameObject.SetActive(false);
         }
-
+        
         SpawnRPCHelper();
+
+        _goldUI.SetActive(false);
 #endif
     }
 
-    private void SpawnRPCHelper() => photonView.RPC("SpawnHelper", RpcTarget.AllBuffered);
+    private void SpawnRPCHelper(Vector3 playerPos) => photonView.RPC("SpawnHelper", RpcTarget.AllBuffered, playerPos);
 
-    private void SpawnHelper() => StartCoroutine(SpawnObject());
+    private void SpawnHelper(Vector3 playerPos) => StartCoroutine(SpawnObject());
 
     private int _currentIndex = 0;
     private float _elapsedTime = 0f;
@@ -62,5 +72,24 @@ public class SummonCircle : MonoBehaviourPun
         }
 
         _elapsedTime = 0f;
+    }
+
+    private void GetGold(Vector3 playerPos)
+    {
+        StartCoroutine(ShowGoldUI(playerPos));
+        
+
+
+    }
+
+    private IEnumerator ShowGoldUI(Vector3 playerPos)
+    {
+        _goldUI.transform.LookAt(playerPos);
+
+        _goldUI.SetActive(true);
+
+        yield return SPAWN_DELAY;
+
+        _goldUI.SetActive(false);
     }
 }
