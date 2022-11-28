cce4734ae95e8f0581415c10b2d665233dad79d8
qwas4689 <qwas4689@gmail.com>
2022-11-28 18:33:16 +0900

    [Fix] 이세계물건베기 로컬화

--- Assets/_Scripts/Lobby1_Interact/Tutorial1/Tu6_SummonCircle.cs
+++ Assets/_Scripts/Lobby1_Interact/Tutorial1/Tu6_SummonCircle.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
 using _DB = Asset.MySql.MySqlSetting;
 using _IRM = Defines.RPC.IsekaiRPCMethodName;
 using UnityEngine.SceneManagement;
 
-public class SummonCircle : MonoBehaviourPun
+public class Tu6_SummonCircle : MonoBehaviour
 {
     [Header("Isekai Objects")]
-    [SerializeField] IsekaiObject[] _objects;
+    [SerializeField] Tu6_IsekaiObject[] _objects;
 
     [Header("UI")]
     [SerializeField] GameObject _goldUI;
@@ -22,7 +21,7 @@ public class SummonCircle : MonoBehaviourPun
 
     private static readonly WaitForSeconds CONGRAT_DELAY = new WaitForSeconds(0.5f);
     private static readonly WaitForSeconds SPAWN_DELAY = new WaitForSeconds(1f);
-    private static readonly WaitForSeconds FIND_PLAYERNETWORKING_DELAY = new WaitForSeconds(2f);
+    
     private static readonly Vector3 FLOAT_POSITION = new Vector3(0f, 1.2f, 0f);
     private static readonly Vector3 WAIT_POSITION = new Vector3(0f, -1.5f, 0f);
     private const float RISE_TIME = 1f;
@@ -30,14 +29,12 @@ public class SummonCircle : MonoBehaviourPun
     private const int PERCENT_TO_POINT = 1; // 원하는 % 수
     private const int EARN_GOLD = 500;
 
-
-    private BasicPlayerNetworking[] _playerNetworkings;
     private BasicPlayerNetworking _playerNetworking;
     private Vector3 _playerPosition = new Vector3();
 
     private void OnEnable()
     {
-        foreach (IsekaiObject obj in _objects)
+        foreach (Tu6_IsekaiObject obj in _objects)
         {
             obj.ObjectSlashed -= SpawnRPCHelper;
             obj.ObjectSlashed += SpawnRPCHelper;
@@ -50,41 +47,17 @@ public class SummonCircle : MonoBehaviourPun
 
         _audioSource.volume = 0f;
 
-        if (PhotonNetwork.IsMasterClient)
-        {
-            SpawnRPCHelper(_playerPosition);
-        }
+        SpawnRPCHelper(_playerPosition);
 
         _goldUI.SetActive(false);
-
-        StartCoroutine(SetPlayerNetworking());
     }
 
     private void OnDisable()
     {
-        foreach (IsekaiObject obj in _objects)
+        foreach (Tu6_IsekaiObject obj in _objects)
         {
             obj.ObjectSlashed -= SpawnRPCHelper;
             obj.ObjectSlashed -= GetGold;
-        }
-    }
-
-    private IEnumerator SetPlayerNetworking()
-    {
-        yield return FIND_PLAYERNETWORKING_DELAY;
-
-        _audioSource.volume = 1f;
-
-        _playerNetworkings = FindObjectsOfType<BasicPlayerNetworking>();
-
-        foreach (var player in _playerNetworkings)
-        {
-            if (player.GetComponent<PhotonView>().IsMine)
-            {
-                _playerNetworking = player;
-
-                break;
-            }
         }
     }
 
@@ -92,29 +65,27 @@ public class SummonCircle : MonoBehaviourPun
     {
         _currentIndex = Random.Range(0, _objects.Length);
 
-        PhotonNetwork.RemoveBufferedRPCs(photonView.ViewID, _IRM.SpawnHelper);
-        photonView.RPC(_IRM.SpawnHelper, RpcTarget.AllBuffered, _currentIndex);
+        SpawnHelper(_currentIndex);
     }
 
-    [PunRPC]
     private void SpawnHelper(int currentIndex) => StartCoroutine(SpawnObject(currentIndex));
-    
+
     private int _currentIndex = 0;
     private float _elapsedTime = 0f;
     private IEnumerator SpawnObject(int currentIndex)
     {
         _audioSource.PlayOneShot(_fallAudioClip);
-     
+
         yield return SPAWN_DELAY;
 
         _audioSource.PlayOneShot(_riseAudioClip);
 
         _objects[currentIndex].gameObject.SetActive(true);
-        
+
         while (_elapsedTime <= RISE_TIME)
         {
             _elapsedTime += Time.deltaTime;
-            
+
             _objects[currentIndex].transform.localPosition = Vector3.Lerp(WAIT_POSITION, FLOAT_POSITION, _elapsedTime);
 
             yield return null;
@@ -125,7 +96,7 @@ public class SummonCircle : MonoBehaviourPun
         _objects[currentIndex].ReturnIsNotFlick();
     }
 
-    
+
     private void GetGold(Vector3 playerPos)
     {
         if (PERCENT_TO_POINT < Random.Range(1, MAX_TO_HIT))
