7c35274d8fb1fa2ebba51d528aa06145af19fa8f
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-11-28 12:00:13 +0900

    [Fix] 투기장 튜토리얼 로직 변경

--- Assets/_Scripts/Arena/Tutorial/ArenaTutorial.cs
+++ Assets/_Scripts/Arena/Tutorial/ArenaTutorial.cs
@@ -13,12 +13,12 @@ using _CSV = Asset.ParseCSV.CSVParser;
 public class ArenaTutorial : MonoBehaviour
 {
     [Header("Tutorial Text")]
-    [SerializeField] TextMeshProUGUI _conversationText;
-    [SerializeField] GameObject _conversationUI;
-    [SerializeField] AudioClip _dialogueSound;
+    [SerializeField] private TextMeshProUGUI _conversationText;
+    [SerializeField] private GameObject _conversationUI;
+    [SerializeField] private AudioClip _dialogueSound;
 
     [Header("Restart Trigger")]
-    [SerializeField] TutorialBettingUI _tutorialBettingUI;
+    [SerializeField] private TutorialBettingUI _tutorialBettingUI;
 
     private AudioSource _audioSource;
     private List<string> _conversationList = new List<string>();
@@ -37,6 +37,9 @@ public class ArenaTutorial : MonoBehaviour
     void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+
+        
+        _tutorialBettingUI.BettingPanelOff();
 
         if (_isFirstVisit)
         {
@@ -67,35 +70,28 @@ public class ArenaTutorial : MonoBehaviour
 
         }
 
-        if (_indexNum == _pauseNum)
-        {
-            ConversationPause();
-        }
 
-        if (_isPause == false)
+        if (_isPause == false && _indexNum < _pauseNum + 1)
         {
             ConversationSkip();
         }
+        Debug.Log(_conversationList.Count);
+        Debug.Log(_indexNum + _conversationList[_indexNum]);
 
-        if (_indexNum > _conversationList.Count)
-        {
-            TutorialEnd();
-        }
+
     }
     private IEnumerator ConversationPrint()
     {
-
         for (int i = 0; i < _conversationList[_indexNum].Length; ++i)
         {
             yield return new WaitForSeconds(0.1f);
             _conversationText.text += _conversationList[_indexNum][i];
         }
-
     }
 
     private void ConversationSkip()
     {
-        if (_conversationText.text != null)
+        if (_conversationText.text != null && _conversationUI.activeSelf)
         {
             if (_conversationText.text.Length != _conversationList[_indexNum].Length)
             {
@@ -103,36 +99,53 @@ public class ArenaTutorial : MonoBehaviour
                 {
                     StopCoroutine(ConversationCoroutine);
                     _conversationText.text = _conversationList[_indexNum];
+
                 }
             }
             else
             {
                 if (OVRInput.GetDown(OVRInput.RawButton.A))
                 {
+                    if (_indexNum == _pauseNum)
+                    {
+                        ConversationPause();
+                    }
+
+                    if (_indexNum == _conversationList.Count - 1)
+                    {
+                        TutorialEnd();
+                    }
+
                     ++_indexNum;
 
-                    if (_indexNum <= _conversationList.Count)
-                    {
-                        _audioSource.PlayOneShot(_dialogueSound);
-                        _conversationText.text = null;
-                        ConversationCoroutine = StartCoroutine(ConversationPrint());
-                    }
+
+                    _audioSource.PlayOneShot(_dialogueSound);
+                    _conversationText.text = null;
+                    ConversationCoroutine = StartCoroutine(ConversationPrint());
+
                 }
             }
+
+
         }
+
+
     }
 
     private void ConversationPause()
     {
+        _tutorialBettingUI.transform.GetChild(0).gameObject.SetActive(true);
+
         _isPause = true;
         _conversationUI.SetActive(false);
     }
 
     private void ConversationRestart()
     {
-        _tutorialBettingUI.BettingPanelOff();
-        _isPause = false;
+        _pauseNum = 19;
+        _tutorialBettingUI.transform.GetChild(0).gameObject.SetActive(false);
         _conversationUI.SetActive(true);
+        _isPause = false;
     }
 
     private void TutorialEnd() => SceneManager.LoadScene((int)SceneType.ArenaRoom);
