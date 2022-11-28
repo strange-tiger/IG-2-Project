87ac54e56ac5e8f013e07c2ba80a5d0237724e2c
csy-59 <csy010509@naver.com>
2022-11-28 20:17:39 +0900

    [Feat] 캠프파이어 퀘스트 완료

--- Assets/_Scripts/Tutorial/Lobby2Tutorial/QuestConducters/4_Campfire/CampfireQuest.cs
+++ Assets/_Scripts/Tutorial/Lobby2Tutorial/QuestConducters/4_Campfire/CampfireQuest.cs
@@ -5,10 +5,37 @@ using UnityEngine;
 public class CampfireQuest : QuestConducter
 {
     [SerializeField] private GameObject _props;
+    private WoodPileForTutorial _woodPile;
+
+    [SerializeField] private int _woodCountForQuestEnd = 3;
+
+    [SerializeField] private int _woodCount = 0;
+
+    private void Awake()
+    {
+        _woodPile = _props.GetComponentInChildren<WoodPileForTutorial>();
+    }
 
     public override void StartQuest()
     {
+        _woodPile.ResetWood();
+        _woodCount = 0;
         _props.SetActive(true);
+    }
+
+    public void StackWood()
+    {
+        ++_woodCount;
+
+        if(_woodCount >= _woodCountForQuestEnd)
+        {
+            QuestEnded();
+        }
+    }
+
+    public void WoodOut()
+    {
+        _woodCount = _woodCount > 0 ? _woodCount - 1 : 0;
     }
 
     private void OnDisable()
---
+++ Assets/_Scripts/Tutorial/Lobby2Tutorial/QuestConducters/4_Campfire/WoodPileForTutorial.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodPileForTutorial : InteracterableObject
+{
+    private WoodForTutorial[] _woods;
+    private int _nextWood = 0;
+
+    private static readonly YieldInstruction INTERACT_COOLTIME = new WaitForSeconds(3f);
+    private static readonly Vector3[] SPAWN_DIRECTION = new Vector3[4] { Vector3.forward, Vector3.back, Vector3.right, Vector3.left };
+    private const float SPAWN_WOOD_FORCE = 1f;
+    private bool _onCooltime = false;
+
+    private static readonly Vector3 _originalWoodPosition = new Vector3(0f, 0.5f, 0f);
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _woods = GetComponentsInChildren<WoodForTutorial>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        if(_onCooltime)
+        {
+            return;
+        }
+
+        SpawnWood();
+    }
+
+    private void SpawnWood()
+    {
+        Vector3 spawnDirection = 2f * Vector3.up + SPAWN_DIRECTION[Random.Range(0, 4)];
+
+        GameObject wood = _woods[_nextWood].gameObject;
+        _nextWood = (_nextWood + 1) % _woods.Length;
+
+        wood.SetActive(true);
+        wood.transform.localPosition = _originalWoodPosition;
+        wood.GetComponent<Rigidbody>().AddForce(SPAWN_WOOD_FORCE * spawnDirection, ForceMode.Impulse);
+
+        StartCoroutine(CalculateCooltime());
+    }
+
+    private IEnumerator CalculateCooltime()
+    {
+        _onCooltime = true;
+
+        yield return INTERACT_COOLTIME;
+
+        _onCooltime = false;
+    }
+
+    public void ResetWood()
+    {
+        foreach (WoodForTutorial wood in _woods)
+        {
+            wood.gameObject.SetActive(false);
+        }
+    }
+}
