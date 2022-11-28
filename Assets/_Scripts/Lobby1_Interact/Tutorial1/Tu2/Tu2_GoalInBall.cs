2da216be08977e051b60af205a5022cc246924c4
qwas4689 <qwas4689@gmail.com>
2022-11-28 17:31:43 +0900

    [Fix] 공놀이, 나와라 마법봉 로컬화 완료

---
+++ Assets/_Scripts/Lobby1_Interact/Tutorial1/Tu2/Tu2_GoalInBall.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class Tu2_GoalInBall : MonoBehaviourPunCallbacks
+{
+    private ParticleSystem[] _goalIn;
+
+    private void Awake()
+    {
+        _goalIn = new ParticleSystem[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            _goalIn[i] = gameObject.transform.GetChild(i).GetComponent<ParticleSystem>();
+            _goalIn[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider _goalLine)
+    {
+        if (photonView.IsMine)
+        {
+            if (_goalLine.gameObject.tag == "Ball" && _goalLine.gameObject.transform.position.y > gameObject.transform.position.y)
+            {
+                PlayGoalInParticle();
+            }
+
+        }
+    }
+
+    private void PlayGoalInParticle()
+    {
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            _goalIn[i].gameObject.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/_Scripts/Lobby1_Interact/Tutorial1/Tu3_MagicWandGrabManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Tu3_MagicWandGrabManager : MonoBehaviourPun
+{
+    [SerializeField]
+    private Tu3_MagicWand _magicWand;
+
+    private SyncOVRDistanceGrabbable _syncOVRDistanceGrabbable;
+
+    private bool _isReady;
+
+    private void Start()
+    {
+        _magicWand.enabled = false;
+        _syncOVRDistanceGrabbable = GetComponent<SyncOVRDistanceGrabbable>();
+    }
+
+    private void Update()
+    {
+        if (_syncOVRDistanceGrabbable.isGrabbed == true && !_isReady)
+        {
+            _magicWand.enabled = true;
+            _isReady = true;
+        }
+        else if (_syncOVRDistanceGrabbable.isGrabbed == false && _isReady)
+        {
+            _magicWand.enabled = false;
+            _isReady = false;
+        }
+    }
+}
