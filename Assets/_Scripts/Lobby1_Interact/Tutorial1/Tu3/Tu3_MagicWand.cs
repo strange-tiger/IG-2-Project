5eab90e79be3d9dfe0710cc198df222fd5042cd1
qwas4689 <qwas4689@gmail.com>
2022-11-29 19:56:49 +0900

    [Fix] 튜토리얼 : 로비1 나와라 마법봉 로직 수정
    이제 쿨타임이 남아 있을 때 UI가 나옵니다

--- Assets/_Scripts/Lobby1_Interact/Tutorial1/Tu3/Tu3_MagicWand.cs
+++ Assets/_Scripts/Lobby1_Interact/Tutorial1/Tu3/Tu3_MagicWand.cs
@@ -76,16 +76,11 @@ public class Tu3_MagicWand : MonoBehaviour
         if (_checkCoolTime)
         {
             _currentTime += Time.deltaTime;
-
-            _coolTimeText = (int)_coolTime;
-
-            _coolTimeText -= (int)_currentTime;
             _magicCoolTimeText.text = _coolTimeText.ToString();
 
             if (_currentTime > (float)_coolTime)
             {
                 _currentTime -= _currentTime;
-                _checkCoolTime = false;
                 _magicWandPanel.SetActive(false);
             }
         }
