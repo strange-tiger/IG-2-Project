4e462eed270612a445f1931424e43ecbe0522d4f
csy-59 <csy010509@naver.com>
2022-12-01 17:09:49 +0900

    [Fix] 튜토리얼 비어 콜라이더 문제 해결

--- Assets/_Scripts/Tutorial/Lobby2Tutorial/QuestConducters/2_WesternBeerTutorial/BeerForTutorial.cs
+++ Assets/_Scripts/Tutorial/Lobby2Tutorial/QuestConducters/2_WesternBeerTutorial/BeerForTutorial.cs
@@ -14,12 +14,18 @@ public class BeerForTutorial : MonoBehaviour
     private AudioSource _audioSource;
     private Rigidbody _rigidbody;
 
+    private SyncOVRGrabbable _syncOVRGrabbable;
+
     private void Awake()
     {
         _initBeerPosition = transform.position;
         _audioSource = GetComponent<AudioSource>();
         _rigidbody = GetComponent<Rigidbody>();
         _myCollider = GetComponent<BoxCollider>();
+
+        _syncOVRGrabbable = GetComponent<SyncOVRGrabbable>();
+        _syncOVRGrabbable.CallbackOnGrabBegin = OnGrabBegin;
+        _syncOVRGrabbable.CallbackOnGrabEnd = OnGrabEnd;
     }
 
     private void OnEnable()
@@ -40,5 +46,19 @@ public class BeerForTutorial : MonoBehaviour
         _grabCollider.enabled = false;
         _myCollider.enabled = false;
         _rigidbody.useGravity = false;
+    }
+
+    private void OnGrabBegin()
+    {
+        Debug.Log("[Beer] ±×·¦ ½ÃÀÛµÊ");
+        _myCollider.isTrigger = true;
+        _beerCollider.isTrigger = true;
+    }
+
+    private void OnGrabEnd()
+    {
+        Debug.Log("[Beer] ±×·¦ ³¡³²");
+        _myCollider.isTrigger = false;
+        _beerCollider.isTrigger = false;
     }
 }
