d992d6b09753e4d87d4ec7eceb767e468511b3cb
qwas4689 <qwas4689@gmail.com>
2022-12-01 12:48:09 +0900

    [Fix] 오크통 버그픽스 / 테스트용

--- Assets/_Scripts/Player/PlayerInteraction.cs
+++ Assets/_Scripts/Player/PlayerInteraction.cs
@@ -18,6 +18,8 @@ public class PlayerInteraction : MonoBehaviourPun
     private bool _isOak;
     public UnityEvent InteractionOakBarrel = new UnityEvent();
 
+    private OakBarrelInteraction _oakBarrelInteraction;
+
     private void OnEnable()
     {
         _eventSystemInputModule = transform.root.GetComponentInChildren<OVRInputModule>();
@@ -35,6 +37,7 @@ public class PlayerInteraction : MonoBehaviourPun
             _ovrRaycaster.pointer = _pointer.gameObject;
             _isThereUI = true;
         }
+        _oakBarrelInteraction = GetComponentInParent<OakBarrelInteraction>();
     }
 
     private void Update()
@@ -129,20 +132,12 @@ public class PlayerInteraction : MonoBehaviourPun
                 interacterableObject.Interact();
                 Debug.Log(interacterableObject.name);
 
-                if (!photonView.IsMine)
+                if (!photonView.IsMine && !_oakBarrelInteraction.IsInOak)
                 {
                     if (interacterableObject.CompareTag("OakBarrel"))
                     {
                         InteractionOakBarrel.Invoke();
                     }
-
-                    //if (interacterableObject.CompareTag("Player"))
-                    //{
-                    //    OakBarrelInteraction _oakBarrelInteraction;
-                    //    _oakBarrelInteraction = interacterableObject.transform.root.gameObject.GetComponentInParent<OakBarrelInteraction>();
-
-                    //    InteractionOakBarrel.Invoke();
-                    //}
                 }
             }
         }
