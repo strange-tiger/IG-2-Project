173104bf5e8704ab9a83fb4a5994a9bae3fabdba
qwas4689 <qwas4689@gmail.com>
2022-12-02 15:58:15 +0900

    [Fix] 오크통 수정

--- Assets/_Scripts/OakBarrel/OakBarrel.cs
+++ Assets/_Scripts/OakBarrel/OakBarrel.cs
@@ -19,6 +19,8 @@ public class OakBarrel : InteracterableObject
     {
         _oakBarrelMeshRenderer = GetComponent<MeshRenderer>();
         _oakBarrelMeshCollider = GetComponent<MeshCollider>();
+
+        photonView.RPC(nameof(SomeoneInteractedOakBarrel), RpcTarget.AllBuffered, _isPlayerHave);
     }
 
     public override void Interact()
@@ -40,6 +42,12 @@ public class OakBarrel : InteracterableObject
         _oakBarrelMeshRenderer.enabled = value;
         _oakBarrelMeshCollider.enabled = value;
         Debug.Log($"{photonView.IsMine}오크통과 상호작용 RPC 뿌리기");
+    }
+
+    [PunRPC]
+    private void IHaveOakBarrel(bool value)
+    {
+        _isPlayerHave = value;
     }
 
     private IEnumerator SetOakBarrelOriginalPosition()
