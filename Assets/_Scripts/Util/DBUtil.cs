e83ffb728f4c4b2a7eb89fd2be007558922f8ebd
Klistas <fukoyjh@gmail.com>
2022-12-02 15:21:45 +0900

    [Fix] 골드 분배 로직 변경.

--- Assets/_Scripts/Util/DBUtil.cs
+++ Assets/_Scripts/Util/DBUtil.cs
@@ -1121,16 +1121,14 @@ namespace Asset.MySql
         {
             try
             {
-
-                winnerListDictionary.Clear();
-
-                string selectAllBettingData = SelectDBHelper(ETableType.bettingdb) + $" where BettingChampionNumber = '{winChampionNumber}'";
-
-                string selectDrawBettingData = SelectDBHelper(ETableType.bettingdb);
-
                 using (MySqlConnection _mysqlConnection = new MySqlConnection(_connectionString))
                 {
-                    _mysqlConnection.Open();
+                    winnerListDictionary.Clear();
+
+                    string selectAllBettingData = SelectDBHelper(ETableType.bettingdb) + $" where BettingChampionNumber = '{winChampionNumber}'";
+
+                    string selectDrawBettingData = SelectDBHelper(ETableType.bettingdb);
+
 
                     if (isDraw)
                     {
@@ -1138,6 +1136,8 @@ namespace Asset.MySql
 
                         foreach (DataRow _dataRow in bettingDBdata.Tables[0].Rows)
                         {
+                            _mysqlConnection.Open();
+                            
                             int betGold = (int)double.Parse(_dataRow[EbettingdbColumns.BettingGold.ToString()].ToString());
 
                             int haveGold = int.Parse(_dataRow["HaveGold"].ToString()) + betGold;
@@ -1147,6 +1147,9 @@ namespace Asset.MySql
                             MySqlCommand command = new MySqlCommand(updateString, _mysqlConnection);
 
                             command.ExecuteNonQuery();
+
+                            _mysqlConnection.Close();
+
                         }
                         OnBettingDraw.Invoke();
                     }
@@ -1154,9 +1157,10 @@ namespace Asset.MySql
                     {
                         DataSet bettingDBdata = GetUserData(selectAllBettingData);
 
-
                         foreach (DataRow _dataRow in bettingDBdata.Tables[0].Rows)
                         {
+                            _mysqlConnection.Open();
+
                             int betGold = Convert.ToInt32(Math.Round(((Convert.ToDouble(betAmount) * (double.Parse(_dataRow[EbettingdbColumns.BettingGold.ToString()].ToString()) / Convert.ToDouble(championBetAmount)))
                                 )));
 
@@ -1168,10 +1172,12 @@ namespace Asset.MySql
 
                             MySqlCommand command = new MySqlCommand(updateString, _mysqlConnection);
                             command.ExecuteNonQuery();
+
+                            _mysqlConnection.Close();
+
                         }
 
                     }
-                    _mysqlConnection.Close();
                 }
 
                 return winnerListDictionary;
