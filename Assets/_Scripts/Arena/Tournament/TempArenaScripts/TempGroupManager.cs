a3b15f58131a03424ad926786ffa9b5100acf9bb
qwas4689 <qwas4689@gmail.com>
2022-12-18 16:52:52 +0900

    [Refactor] 리펙토링 중

--- Assets/_Scripts/Arena/Tournament/TempArenaScripts/TempGroupManager.cs
+++ Assets/_Scripts/Arena/Tournament/TempArenaScripts/TempGroupManager.cs
@@ -23,12 +23,7 @@ public class TempGroupManager : MonoBehaviour
     private Vector3 _oddChampionPostion = new Vector3(8.5f, 0f, 0f);
     private Vector3 _oddChampionRotation = new Vector3(0f, -90f, 0f);
 
-    private const int _zero = 0;
-    private const int _one = 1;
-    private const int _two = 2;
-    private const float _half = 0.5f;
-
-    private static bool _battleValue = true;
+    private bool _battleValue = true;
 
     private int _randGroupIndex;
 
@@ -54,20 +49,19 @@ public class TempGroupManager : MonoBehaviour
     private void LuckyWinnerSet(List<GameObject> list)
     {
         int championsNum = list.Count;
-        int championsIndex = championsNum - _one;
+        int championsIndex = championsNum - 1;
 
-        if (championsNum % _two != _zero)
+        if (championsNum % 2 != 0)
         {
-            if (!Any(_luckyGroup))
+            if (!IsEmpty(_luckyGroup))
             {
-                list.Add(_luckyGroup[_zero]);
+                list.Add(_luckyGroup[0]);
                 _luckyGroup.Clear();
                 return;
             }
             _luckyGroup.Add(list[championsIndex]);
             list.RemoveAt(championsIndex);
         }
-        
     }
 
     /// <summary>
@@ -79,16 +73,16 @@ public class TempGroupManager : MonoBehaviour
         int championsNum = transform.childCount;
 
         // 챔피언들을 랜덤하게 배치하기 위한 랜덤값 넣어주기
-        for (int i = _zero; i < transform.childCount; ++i)
+        for (int i = 0; i < transform.childCount; ++i)
         {
-            _randGroupIndex = UnityEngine.Random.Range(_zero, championsNum);
+            _randGroupIndex = UnityEngine.Random.Range(0, championsNum);
 
-            for (int j = _zero; j < _groupList.Count;)
+            for (int j = 0; j < _groupList.Count;)
             {
                 if (_groupList[j] == _randGroupIndex)
                 {
-                    j = _zero;
-                    _randGroupIndex = UnityEngine.Random.Range(_zero, championsNum);
+                    j = 0;
+                    _randGroupIndex = UnityEngine.Random.Range(0, championsNum);
                 }
 
                 else
@@ -109,7 +103,7 @@ public class TempGroupManager : MonoBehaviour
     {
         int even = 0;
         int odd = 1;
-        for (int i = _zero; i < list.Count * _half; ++i)
+        for (int i = 0; i < list.Count * 0.5; ++i)
         {
             list[even].transform.position = _evenChampionPostion;
             list[even].transform.rotation = Quaternion.Euler(_evenChampionRotation);
@@ -117,8 +111,8 @@ public class TempGroupManager : MonoBehaviour
             list[odd].transform.position = _oddChampionPostion;
             list[odd].transform.rotation = Quaternion.Euler(_oddChampionRotation);
 
-            even += _two;
-            odd += _two;
+            even += 2;
+            odd += 2;
         }
     }
 
@@ -135,7 +129,7 @@ public class TempGroupManager : MonoBehaviour
 
         while (true)
         {
-            if (Any(list))
+            if (IsEmpty(list))
             {
                 // 부전승 판정들
                 LuckyWinnerSet(winnerList);
@@ -154,11 +148,11 @@ public class TempGroupManager : MonoBehaviour
                 }
                 yield return _waitTime;
 
-                if (list.Count > _one)
+                if (list.Count > 1)
                 {
                     _battleValue = true;
                 }
-                else if (list.Count == _one && !Any(_luckyGroup))
+                else if (list.Count == 1 && !IsEmpty(_luckyGroup))
                 {
                     continue;
                 }
@@ -187,11 +181,11 @@ public class TempGroupManager : MonoBehaviour
                 }
                 yield return _waitTime;
 
-                if (winnerList.Count > _one)
+                if (winnerList.Count > 1)
                 {
                     _battleValue = true;
                 }
-                else if (winnerList.Count == _one && !Any(_luckyGroup))
+                else if (winnerList.Count == 1 && !IsEmpty(_luckyGroup))
                 {
                     continue;
                 }
@@ -208,9 +202,9 @@ public class TempGroupManager : MonoBehaviour
     /// </summary>
     /// <param name="list">확인할 리스트</param>
     /// <returns></returns>
-    private bool Any(List<GameObject> list)
+    private bool IsEmpty(List<GameObject> list)
     {
-        if (list.Count == _zero)
+        if (list.Count == 0)
         {
             return true;
 
@@ -224,7 +218,7 @@ public class TempGroupManager : MonoBehaviour
     /// <param name="fightGroup">전투시킬 리스트 넣어주기</param>
     private void FightGroup(List<GameObject> fightGroup)
     {
-        if (!Any(fightGroup))
+        if (!IsEmpty(fightGroup))
         {
             fightGroup[_evenIndex].SetActive(true);
             fightGroup[_oddIndex].SetActive(true);
@@ -239,9 +233,9 @@ public class TempGroupManager : MonoBehaviour
     /// 경기 중 누군가 죽었을 때 이루어질 동작들
     /// </summary>
     /// <param name="fightGroup">싸우고있는 그룹</param>
-    /// <param name="winerGroup">승자들을 넣어줄 그룹</param>
+    /// <param name="winnerGroup">승자들을 넣어줄 그룹</param>
     /// <returns></returns>
-    private bool SomeOneDie(List<GameObject> fightGroup, List<GameObject> winerGroup)
+    private bool SomeOneDie(List<GameObject> fightGroup, List<GameObject> winnerGroup)
     {
         if (_evenIndex == fightGroup.Count || _oddIndex == fightGroup.Count)
         {
@@ -252,25 +246,28 @@ public class TempGroupManager : MonoBehaviour
         {
             if (fightGroup[_evenIndex].activeSelf == false)
             {
-                winerGroup.Add(fightGroup[_oddIndex]);
+                winnerGroup.Add(fightGroup[_oddIndex]);
                 fightGroup[_oddIndex].SetActive(false);
-                fightGroup.RemoveAt(_zero);
-                fightGroup.RemoveAt(_zero);
+                fightGroup.RemoveRange(0, 2);
 
                 return true;
             }
             else if (fightGroup[_oddIndex].activeSelf == false)
             {
-                winerGroup.Add(fightGroup[_evenIndex]);
+                winnerGroup.Add(fightGroup[_evenIndex]);
                 fightGroup[_evenIndex].SetActive(false);
-                fightGroup.RemoveAt(_zero);
-                fightGroup.RemoveAt(_zero);
+                fightGroup.RemoveRange(0, 2);
 
                 return true;
             }
         }
 
         return false;
+    }
+
+    private void SetGroup(int num)
+    {
+        
     }
 
     /// <summary>
